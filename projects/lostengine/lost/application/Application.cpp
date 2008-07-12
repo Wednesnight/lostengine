222c71e46ff9a146bc7e4f1241f429c1bd41eeed
lobotony <lobotony666@yahoo.de>
2008-07-12 14:27:10 +0000

    added preliminary lifecycle messages

--- projects/lostengine/lost/application/Application.cpp
+++ projects/lostengine/lost/application/Application.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include "lost/application/Timer.h"
 #include <boost/bind.hpp>
+#include "lost/application/ApplicationEvent.h"
 
 using namespace std;
 using namespace boost;
@@ -16,16 +17,36 @@ namespace application
   Application::Application()
   : adapter(this)
   {
-    adapter.init(displayAttributes);
+    // make sure the global application pointer is set to a correct value
     if(appInstance != NULL)
     {
       throw std::runtime_error("appInstance wasn't NULL, did you instance more than one app?");
     }
     appInstance = this;
+
+    // the main loop is not running yet
     running = false;
+
+    // init default resource loader
+    // init lua state with resource loader
+    // try to load default lua script
+    
+    // broadcast preinit event, this is the latest point for client code to setup the configuration
+    shared_ptr<ApplicationEvent> appEvent(new ApplicationEvent(ApplicationEvent::PREINIT()));dispatchEvent(appEvent);
+    
+    // try to extract default display attributes from lua state
+    
+    // init display/adapter so all other code is safe from here on because an OpenGL context now exists
+    adapter.init(displayAttributes);
+    
+    // broadcast init event so dependant code knows its safe to init resources now
+    appEvent->type = ApplicationEvent::INIT();dispatchEvent(appEvent);
 
     // connect resize callback to ourselves so we can keep track of the window size in the displayAttributes
     addEventListener(ResizeEvent::MAIN_WINDOW_RESIZE(), event::receive<ResizeEvent>(bind(&Application::handleResize, this, _1)));
+
+    // broadcast postinit event to signal that the application initialisation has finished
+    appEvent->type = ApplicationEvent::POSTINIT();dispatchEvent(appEvent);
   }
   
   Application::~Application() { adapter.terminate(); }
@@ -38,6 +59,7 @@ namespace application
 
   void Application::run()
   {
+    appEvent->type = ApplicationEvent::RUN();dispatchEvent(appEvent);
     running = true;
     double lastTime = adapter.getTime();
     while(running)
@@ -53,9 +75,9 @@ namespace application
         running = false;
       }
     }
+    appEvent->type = ApplicationEvent::QUIT();dispatchEvent(appEvent);
   }
 
   void Application::quit() { running=false; }
-
 }
 }
