155e7585825f9fb73d6ff5298aa95a82295e321f
lobotony <lobotony666@yahoo.de>
2008-07-23 19:10:03 +0000

    ported Buff0r to new application framework

--- sandbox/Buff0r/Buff0r.h
+++ sandbox/Buff0r/Buff0r.h
@@ -6,9 +6,12 @@
 #include "lost/gl/Utils.h"
 #include "lost/common/Color.h"
 #include "lost/math/Vec2.h"
-#include "lost/application/Timer.h"
+#include "lost/application/TimerEvent.h"
+#include "lost/application/KeyEvent.h"
+#include "lost/application/ApplicationEvent.h"
+#include "lost/application/ResizeEvent.h"
 #include "lost/common/DisplayAttributes.h"
-#include "lost/engine/KeySym.h"
+#include "lost/application/KeySym.h"
 #include "lost/common/FpsMeter.h"
 #include "lost/gl/ShaderHelper.h"
 #include <boost/shared_ptr.hpp>
@@ -30,55 +33,68 @@ struct Buff0r
   float                   passed;
 
   boost::shared_ptr<lost::gl::ShaderProgram>      program;
-  lost::gl::FrameBuffer frameBuffer;
-  lost::gl::Texture depthTexture;
-  lost::gl::Texture colorTexture;
+  boost::shared_ptr<lost::gl::FrameBuffer> frameBuffer;
+  boost::shared_ptr<lost::gl::Texture> depthTexture;
+  boost::shared_ptr<lost::gl::Texture> colorTexture;
   int frameBufferWidth;
   int frameBufferHeight;
-
-  Buff0r(lost::common::DisplayAttributes& inDisplayAttributes)
+  boost::shared_ptr<lost::resource::Loader> loader;
+  
+  Buff0r(lost::common::DisplayAttributes& inDisplayAttributes, boost::shared_ptr<lost::resource::Loader> inLoader)
   : eye(0,0,-5),
     at(0,0,0),
     up(0,1,0),
-    displayAttributes(inDisplayAttributes)
+    displayAttributes(inDisplayAttributes),
+    loader(inLoader)
   {
     fovy = 90;
     znear = 1;
     zfar = 100;
-    passed = 0;
-    
-    frameBufferInit();
-    shaderInit();
+    passed = 0;    
   }
 
+  void init(boost::shared_ptr<lost::event::Event> event)
+  {
+    frameBuffer.reset(new lost::gl::FrameBuffer);
+    depthTexture.reset(new lost::gl::Texture);
+    colorTexture.reset(new lost::gl::Texture);
+
+    frameBufferInit();
+    shaderInit();
+    if(GLEE_EXT_framebuffer_object)
+    {  DOUT("Framebuffer objects OK");}
+    else
+      DOUT("NO framebuffer objects");
+  }
+  
   void frameBufferInit()
   {
     frameBufferWidth = 512;
     frameBufferHeight = 512;
 
-    frameBuffer.enable();
+    frameBuffer->enable();
 
-    depthTexture.bind();
-    depthTexture.reset(0,GL_DEPTH_COMPONENT32,frameBufferWidth, frameBufferHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-    frameBuffer.attachDepth(depthTexture);
+    depthTexture->bind();
+    depthTexture->reset(0,GL_DEPTH_COMPONENT32,frameBufferWidth, frameBufferHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+    frameBuffer->attachDepth(*depthTexture);
 
-    colorTexture.bind();
-    colorTexture.reset(0,GL_RGBA8,frameBufferWidth, frameBufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-    frameBuffer.attachColor(0, colorTexture);
+    colorTexture->bind();
+    colorTexture->reset(0,GL_RGBA8,frameBufferWidth, frameBufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+    frameBuffer->attachColor(0, *colorTexture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // GL_NEAREST to switch off filtering
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     int clampparam = GL_CLAMP; // clamp the texture, don't repeat it if the coords are larger than 1s
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clampparam);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, clampparam);
 
-    if(!frameBuffer.status() == GL_FRAMEBUFFER_COMPLETE_EXT)
-      throw std::runtime_error("FrameBuffer status: "+lost::gl::utils::enum2string(frameBuffer.status()));
-    frameBuffer.disable();
+    if(!frameBuffer->status() == GL_FRAMEBUFFER_COMPLETE_EXT)
+      throw std::runtime_error("FrameBuffer status: "+lost::gl::utils::enum2string(frameBuffer->status()));
+    frameBuffer->disable();
   }
 
   void shaderInit()
   {
-    program = lost::gl::loadShader("lattice");
+    program = lost::gl::loadShader(loader, "lattice");
     program->enable();
     program->validate();
     if(!program->validated())
@@ -101,18 +117,18 @@ struct Buff0r
     program->disable();
   }
 
-  void resetViewPort(int width, int height)
+  void resetViewPort(boost::shared_ptr<lost::application::ResizeEvent> event)
   {
-    glViewport(0, 0, width-1, height-1);GLDEBUG;
+    glViewport(0, 0, event->width-1, event->height-1);GLDEBUG;
     lost::gl::utils::set3DProjection(displayAttributes.width, displayAttributes.height, eye, at, up, fovy, znear, zfar);
   }
 
-  void keyboard( const lost::event::KeyEvent& inEvent )
+  void keyboard(boost::shared_ptr<lost::application::KeyEvent> inEvent )
   {
-    switch (inEvent.key)
+    switch (inEvent->key)
     {
-      case lost::engine::K_ESCAPE :
-        if (!inEvent.pressed)
+      case lost::application::K_ESCAPE :
+        if (!inEvent->pressed)
         {
           quit();
         }
@@ -122,7 +138,7 @@ struct Buff0r
     }
   }
 
-  void redraw(double deltaSec, lost::application::Timer* timer)
+  void redraw(boost::shared_ptr<lost::application::TimerEvent> event)
   {
       glClearColor( 0.0, 0.0, 0.0, 0.0 );GLDEBUG;
       glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );GLDEBUG;
@@ -131,7 +147,7 @@ struct Buff0r
       glDisable(GL_TEXTURE_2D);GLDEBUG;
     // enable framebuffer and set viewport
     {
-      frameBuffer.enable();
+      frameBuffer->enable();
       glClearColor( 0.0, 0.0, 0.0, 0.0 );GLDEBUG;
       glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );GLDEBUG;
       lost::gl::PushAttrib pushAttrib(GL_VIEWPORT_BIT);
@@ -140,7 +156,7 @@ struct Buff0r
       lost::gl::utils::set3DProjection(frameBufferWidth, frameBufferHeight, eye, at, up, fovy, znear, zfar);
       glColor3f(1, 1, 0);
       program->enable();
-      passed += deltaSec;
+      passed += event->passedSec;
       glMatrixMode(GL_MODELVIEW);GLDEBUG;
       glLoadIdentity();GLDEBUG;
       glRotatef(180*sin(passed), 0,1,0);
@@ -152,7 +168,7 @@ struct Buff0r
       glutSolidCube(1);
 
       program->disable();
-      frameBuffer.disable();
+      frameBuffer->disable();
     }
     
   
@@ -168,7 +184,7 @@ struct Buff0r
 //    glRotatef(15, 0,0,1);
     glColor3f(1, 1, 1);
     lost::math::Rect rect(50, 50, 512, 512);
-    colorTexture.bind();
+    colorTexture->bind();
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0);glVertex2f(rect.x, rect.y);
     glTexCoord2f(1, 0);glVertex2f(rect.x+rect.width-1, rect.y);
@@ -180,7 +196,7 @@ struct Buff0r
 
     glColor4f(1, 1, 1, .5);
     rect = lost::math::Rect(60, 50, 512, 512);
-    colorTexture.bind();
+    colorTexture->bind();
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0);glVertex2f(rect.x, rect.y);
     glTexCoord2f(1, 0);glVertex2f(rect.x+rect.width-1, rect.y);
@@ -191,7 +207,7 @@ struct Buff0r
     glDisable(GL_TEXTURE_2D);GLDEBUG;
 
     glLoadIdentity();GLDEBUG;
-    fpsMeter.render(1, 0, deltaSec);
+    fpsMeter.render(1, 0, event->passedSec);
     glfwSwapBuffers();
   }
 
--- sandbox/Buff0r/main.cpp
+++ sandbox/Buff0r/main.cpp
@@ -1,30 +1,34 @@
 #include "lost/common/Logger.h"
 #include "lost/application/Application.h"
+#include "lost/application/ApplicationEvent.h"
 #include "lost/application/Timer.h"
+#include "lost/application/TimerEvent.h"
+#include "lost/application/KeyEvent.h"
+#include "lost/application/ResizeEvent.h"
+
 #include "Buff0r.h"
 #include <boost/bind.hpp>
 
 using namespace std;
 using namespace lost::common;
 using namespace lost::application;
+using namespace boost;
+using namespace lost::event;
 
 int main(int argn, char** args)
 {
   LogLevel( log_all );
   try
   {
-    Application app;
-    if(GLEE_EXT_framebuffer_object)
-    {  DOUT("Framebuffer objects OK");}
-    else
-      DOUT("NO framebuffer objects");
-    
-    Buff0r buff0r(app.displayAttributes);
-    app.key.connect(boost::bind(&Buff0r::keyboard, &buff0r, _1));
-    app.windowResize.connect(boost::bind(&Buff0r::resetViewPort, &buff0r, _1,_2));
-    buff0r.quit.connect(boost::bind(&Application::quit, &app));
-    Timer t1("redrawTimer", 0.015);
-    t1.action.connect(boost::bind(&Buff0r::redraw, &buff0r, _1, _2));
+    Application app;    
+    Buff0r buff0r(app.displayAttributes, app.loader);
+    app.addEventListener(ApplicationEvent::INIT(), bind(&Buff0r::init, &buff0r, _1));
+    app.addEventListener(KeyEvent::KEY_DOWN(), receive<KeyEvent>(bind(&Buff0r::keyboard, &buff0r, _1)));
+    app.addEventListener(ResizeEvent::MAIN_WINDOW_RESIZE(), receive<ResizeEvent>(bind(&Buff0r::resetViewPort, &buff0r, _1)));
+    buff0r.quit.connect(bind(&Application::quit, &app));
+
+    Timer redrawTimer("redrawTimer", 1.0/60.0);
+    redrawTimer.addEventListener(TimerEvent::TIMER_FIRED(), receive<TimerEvent>(bind(&Buff0r::redraw, &buff0r, _1)));
 
     app.run();
   }
