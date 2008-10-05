5cf6d3a726f7d488676b403792b2bb5b31f7bb18
lobotony <lobotony666@yahoo.de>
2008-10-05 22:30:03 +0000

    added missing includes after tidying up

--- projects/lostengine/lost/lua/bindings/LostGLDraw.cpp
+++ projects/lostengine/lost/lua/bindings/LostGLDraw.cpp
@@ -1,5 +1,7 @@
 #include "lost/lua/bindings/LostGLDraw.h"
 #include "lost/gl/Draw.h"
+#include "lost/math/Rect.h"
+#include "lost/common/Color.h"
 
 using namespace luabind;
 using namespace lost::gl;
