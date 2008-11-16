aa0b485e1e744d17c562e6bc6e60d306c9ab7ea3
lobotony <lobotony666@yahoo.de>
2008-11-16 18:52:51 +0000

    messing around

--- projects/lostengine/lost/font/TrueTypeFont.cpp
+++ projects/lostengine/lost/font/TrueTypeFont.cpp
@@ -1,6 +1,10 @@
 #include "lost/font/TrueTypeFont.h"
+#include <stdexcept>
+#include <boost/lexical_cast.hpp>
+#include "lost/common/Logger.h"
 
 using namespace boost;
+using namespace lost::bitmap;
 using namespace lost::font::freetype;
 using namespace lost::math;
 
@@ -19,9 +23,43 @@ TrueTypeFont::~TrueTypeFont()
 {
 }
 
+boost::shared_ptr<bitmap::Bitmap>
+TrueTypeFont::renderGlyphToBitmap(uint32_t inSizeInPoints,
+                                  char c)
+{
+  FT_Error error = FT_Set_Char_Size(face->face(), 0, inSizeInPoints*64, 72, 72);
+  if(error) { throw std::runtime_error("FT_Set_Char_Size error: " + boost::lexical_cast<std::string>(error));}
+  
+  FT_UInt idx = FT_Get_Char_Index(face->face(), c);
+  FT_Int32 load_flags = 0;
+  error = FT_Load_Glyph(face->face(), idx, load_flags);
+  if(error) {WOUT("FT_Load_Glyph error: " << error);}
+  
+  FT_Render_Mode render_mode = FT_RENDER_MODE_NORMAL;
+  error = FT_Render_Glyph(face->face()->glyph, render_mode);
+  if(error) {WOUT("FT_Render_Glyph error: " << error);}
+  
+  shared_ptr<Bitmap> result(new Bitmap(face->face()->glyph->bitmap.width,
+                                       face->face()->glyph->bitmap.rows,
+                                       Bitmap::COMPONENTS_RGBA,
+                                       Bitmap::COMPONENTS_ALPHA,
+                                       face->face()->glyph->bitmap.buffer));
+  
+  
+  return result;
+}
+  
+  
 shared_ptr<Model> TrueTypeFont::render(const std::string& inText)
 {
   shared_ptr<Model>  result;
+  
+  // --- add characters of incoming string to current character buffer
+  // --- create bitmaps for characters that don't have one yet
+  // --- stuff new bitmaps into atlas, reshuffle atlas if necessary
+  // --- 
+  
+  
   return result;
 }
 
--- projects/lostengine/lost/font/TrueTypeFont.h
+++ projects/lostengine/lost/font/TrueTypeFont.h
@@ -2,14 +2,15 @@
 #define LOST_FONT_TRUETYPEFONT_H
 
 #include <boost/shared_ptr.hpp>
+#include <list>
+#include <string>
 #include "lost/font/freetype/Library.h"
 #include "lost/font/freetype/Face.h"
 #include "lost/resource/File.h"
 #include "lost/font/Model.h"
-#include <string>
 #include "lost/gl/Texture.h"
 #include "lost/math/Vec2.h"
-#include <list>
+#include "lost/bitmap/Bitmap.h"
 
 namespace lost
 {
@@ -21,24 +22,38 @@ struct TrueTypeFont
   {
     Glyph();
     ~Glyph();
-    char character; // the character code frmo which this glyph was generated
     // texture coordinates for drawing a quad
     lost::math::Vec2  bl;
     lost::math::Vec2  br;
     lost::math::Vec2  tl;
     lost::math::Vec2  tr;
-    boost::shared_ptr<gl::Texture> texture; // the texture that holds the rendered glyph
+    boost::shared_ptr<gl::Texture> texture; // the texture that holds the rendered glyph    
   };
   
   TrueTypeFont(boost::shared_ptr<freetype::Library> inLibrary,
                boost::shared_ptr<resource::File> inFile);
   virtual ~TrueTypeFont();
-  
+
+  /** lets freetype render the specified glyph.
+   * All memory management is performed by freetype. The result is placed in the
+   * face->glyph->bitmap structure.
+   */
+  boost::shared_ptr<bitmap::Bitmap>
+  renderGlyphToBitmap(uint32_t inSizeInPoints,
+                      char c);
+    
+    
   boost::shared_ptr<Model> render(const std::string& inText);
   
   boost::shared_ptr<freetype::Face> face;
   boost::shared_ptr<freetype::Library> library;
   std::list<font::Model> models; // the font knows all models that have been rendered with it
+
+  lost::math::Vec2  atlasMinSize;
+  lost::math::Vec2  atlasMaxSize;
+  
+  
+//  std::map<char, std::map<uint32_t, Glyph> > char2size2glyph; 
 };
 }  
 }
