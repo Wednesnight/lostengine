3730d0c30f68de76ab526f44d4ebcb191d0a2cea
lobotony <lobotony666@yahoo.de>
2008-11-16 18:54:03 +0000

    initial import

---
+++ sandbox/altest/main.cpp
@@ -0,0 +1,33 @@
+#include "lost/common/Logger.h"
+#include "lost/al/al.h"
+#include "lost/al/Device.h"
+#include "lost/al/Context.h"
+#include <boost/shared_ptr.hpp>
+
+using namespace lost::common;
+using namespace lost::al;
+using namespace std;
+using namespace boost;
+
+int main (int argc, char * const argv[]) {
+  LogLevel( log_all );
+  DOUT("check 1 2");
+
+  ALDEBUG; // clear last error
+  
+  vector<string> ids = Device::allDeviceIds();
+  DOUT("found "<<ids.size()<<" devices");
+  for(int i=0; i<ids.size(); ++i)
+  {
+    DOUT("Device "<<i<<" : '"<<ids[i]<<"'");
+  }
+  
+  shared_ptr<Device> device(new Device(ids[0]));
+  shared_ptr<Context> context(new Context(device));
+  
+  context->makeCurrent();
+  context->process();
+  context->suspend();
+  
+  return 0;
+}
