4eb68c30211911dd581e6f6d1433318b5a09cc43
lobotony <lobotony666@yahoo.de>
2008-11-18 21:02:09 +0000

    work in progress

--- sandbox/altest/main.cpp
+++ sandbox/altest/main.cpp
@@ -4,12 +4,14 @@
 #include "lost/al/Context.h"
 #include <boost/shared_ptr.hpp>
 
+#include "stb_vorbis.h"
+
 using namespace lost::common;
 using namespace lost::al;
 using namespace std;
 using namespace boost;
 
-int main (int argc, char * const argv[]) {
+int main (int argc, char* const argv[]) {
   LogLevel( log_all );
   DOUT("check 1 2");
 
@@ -28,6 +30,13 @@ int main (int argc, char * const argv[]) {
   context->makeCurrent();
   context->process();
   context->suspend();
+  
+  int error = 0;
+  stb_vorbis* oggfile = stb_vorbis_open_filename("sketch008.ogg", &error, NULL);
+  stb_vorbis_info info = stb_vorbis_get_info(oggfile);
+  stb_vorbis_close(oggfile);
+  
+  DOUT("ok: "<<info.sample_rate);
   
   return 0;
 }
