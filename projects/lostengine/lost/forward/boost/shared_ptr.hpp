293e5904b2cec76e131f0d5eacee226afd34d81e
jomanto <jomanto@gmx.net>
2008-11-21 13:07:19 +0000

    obsolete

--- projects/lostengine/lost/forward/boost/shared_ptr.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef LOST_FORWARD_BOOST_SHARED_PTR_HPP
-#define LOST_FORWARD_BOOST_SHARED_PTR_HPP
-
-namespace boost
-{
-  template <class T> class shared_ptr;
-}
-
-#endif
--- projects/lostengine/lost/forward/lost/lua/lua.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOST_FORWARD_LOST_LUA_LUA_H
-#define LOST_FORWARD_LOST_LUA_LUA_H
-
-struct lua_State;
-
-#endif
