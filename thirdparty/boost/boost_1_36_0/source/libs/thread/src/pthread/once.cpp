59ba214e5c626742a2a2f8c1acc5ba496f2d32c6
lobotony <lobotony666@yahoo.de>
2009-01-08 04:02:36 +0000

    check __STDC_CONSTANT_MACROS before defining it

--- thirdparty/boost/boost_1_36_0/source/libs/thread/src/pthread/once.cpp
+++ thirdparty/boost/boost_1_36_0/source/libs/thread/src/pthread/once.cpp
@@ -3,7 +3,10 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#ifndef __STDC_CONSTANT_MACROS
 #define __STDC_CONSTANT_MACROS
+#endif
+
 #include <boost/thread/once.hpp>
 #include <boost/assert.hpp>
 #include <pthread.h>
