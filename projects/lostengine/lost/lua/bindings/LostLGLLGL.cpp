238d5e5cbda746f5256f3a413c08b190be7a7f4c
jomanto <jomanto@gmx.net>
2009-01-12 14:31:39 +0000

    fixed RenderBuffer in UserInterface to use LGL_DEPTH_COMPONENT instead of device dependant format

--- projects/lostengine/lost/lua/bindings/LostLGLLGL.cpp
+++ projects/lostengine/lost/lua/bindings/LostLGLLGL.cpp
@@ -19,6 +19,7 @@ namespace lost
 
       globals(state)["lgl"]["LGL_FRAMEBUFFER"] = LGL_FRAMEBUFFER;
       globals(state)["lgl"]["LGL_RENDERBUFFER"] = LGL_RENDERBUFFER;
+      globals(state)["lgl"]["LGL_DEPTH_COMPONENT"] = LGL_DEPTH_COMPONENT;
       globals(state)["lgl"]["LGL_DEPTH_COMPONENT16"] = LGL_DEPTH_COMPONENT16;
       globals(state)["lgl"]["LGL_DEPTH_COMPONENT24"] = LGL_DEPTH_COMPONENT24;
       globals(state)["lgl"]["LGL_FRAMEBUFFER_COMPLETE"] = LGL_FRAMEBUFFER_COMPLETE;
--- projects/lostengine/lost/lua/lost/guiro/UserInterface.lua
+++ projects/lostengine/lost/lua/lost/guiro/UserInterface.lua
@@ -49,7 +49,7 @@ function UserInterface:initialize(context)
 
     self.depthBuffer = lost.gl.RenderBuffer()
     self.depthBuffer:enable()
-    self.depthBuffer:storage(lgl.LGL_DEPTH_COMPONENT24, self.colorTexture.width, self.colorTexture.height)
+    self.depthBuffer:storage(lgl.LGL_DEPTH_COMPONENT, self.colorTexture.width, self.colorTexture.height)
     self.renderBuffer:attachDepth(self.depthBuffer)
     self.depthBuffer:disable()
 
@@ -162,7 +162,7 @@ function UserInterface:updateLayout(forceUpdate)
 
       self.depthBuffer = lost.gl.RenderBuffer()
       self.depthBuffer:enable()
-      self.depthBuffer:storage(lgl.LGL_DEPTH_COMPONENT16, self.colorTexture.width, self.colorTexture.height)
+      self.depthBuffer:storage(lgl.LGL_DEPTH_COMPONENT, self.colorTexture.width, self.colorTexture.height)
       self.renderBuffer:attachDepth(self.depthBuffer)
       self.depthBuffer:disable()
 
