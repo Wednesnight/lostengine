1d60b67b1fe90055d77810a9713ee616e9c3afc3
lobotony <lobotony666@yahoo.de>
2009-01-14 20:56:57 +0000

    initial import

---
+++ sandbox/Thread0r/main.cpp
@@ -0,0 +1,24 @@
+#include "lost/application/Application.h"
+#include "lost/application/ApplicationEvent.h"
+#include "lost/event/Receive.h"
+
+using namespace std;
+using namespace boost;
+using namespace lost::common;
+using namespace lost::application;
+using namespace lost::event;
+
+int main(int argn, char** args)
+{
+  try
+  {
+    Application app;
+    app.run();
+  }
+  catch (std::exception& e)
+  {
+    EOUT("exception: " << e.what());
+  }
+  
+  return 0;
+}
