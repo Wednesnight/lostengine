936f62b3016bc199a50aaeb24ff0d96e43a3c638
lobotony <lobotony666@yahoo.de>
2009-01-14 22:10:42 +0000

    first working version

--- sandbox/Thread0r/main.cpp
+++ sandbox/Thread0r/main.cpp
@@ -8,11 +8,31 @@ using namespace lost::common;
 using namespace lost::application;
 using namespace lost::event;
 
+bool running;
+
+void quitEventHandler(boost::shared_ptr<Event> event)
+{
+  running = false;
+}
+
+void mainLoop()
+{
+  DOUT("starting mainLoop");
+  while(running)
+  {
+  }
+  DOUT("stopping mainLoop");
+}
+
 int main(int argn, char** args)
 {
+  running = true;
+
   try
   {
     Application app;
+    app.mainLoop = mainLoop;
+    app.addEventListener(lost::application::ApplicationEvent::QUIT(), quitEventHandler);
     app.run();
   }
   catch (std::exception& e)
