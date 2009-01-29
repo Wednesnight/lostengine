bb2ae5ec1200e7b3fe37e87a9d5a04008fbe16ec
jomanto <jomanto@gmx.net>
2009-01-29 16:07:15 +0000

    added programmatic creation of the application menu

--- projects/lostengine/lost/application/mac/Application.mm
+++ projects/lostengine/lost/application/mac/Application.mm
@@ -37,7 +37,23 @@
   [[NSApplication sharedApplication] terminate: nil];
 }
 
+- (void) quitAction: (id)sender
+{
+  if (parent) parent->quit();
+}
+
 @end
+
+/*************************************************************
+ * taken from http://www.allegro.cc
+ */
+/* This prevents warnings that 'NSApplication might not
+ * respond to setAppleMenu' on OS X > 10.4
+ */
+@interface NSApplication(LostApplication)
+- (void)setAppleMenu: (NSMenu *)menu;
+@end
+/*************************************************************/
 
 namespace lost
 {
@@ -47,6 +63,7 @@ namespace lost
     struct Application::ApplicationHiddenMembers
     {
       NSAutoreleasePool*   pool;
+      NSApplication*       application;
       ApplicationDelegate* delegate;
     };
 
@@ -60,14 +77,54 @@ namespace lost
       // init pool
       hiddenMembers->pool = [[NSAutoreleasePool alloc] init];
 
-      // TODO: init menu
+      // init app
+      hiddenMembers->application = [NSApplication sharedApplication];
 
       // init delegate
       hiddenMembers->delegate = [ApplicationDelegate alloc];
       [hiddenMembers->delegate setParent: this];
       
       // set application delegate
-      [[NSApplication sharedApplication] setDelegate: hiddenMembers->delegate];
+      [hiddenMembers->application setDelegate: hiddenMembers->delegate];
+
+      /*************************************************************
+       * taken from http://www.allegro.cc
+       */
+      /* Load the main menu nib if possible */
+      if (([NSBundle loadNibNamed: @"MainMenu" owner: NSApp] == NO))
+      {
+        /* Didn't load the nib; create a default menu programmatically */
+        NSMenu *menu;
+        NSMenuItem *menu_item, *temp_item;
+        NSString* title = nil;
+        NSDictionary* app_dictionary = [[NSBundle mainBundle] infoDictionary];
+        if (app_dictionary) 
+        {
+          title = [app_dictionary objectForKey: @"CFBundleName"];
+        }
+        if (title == nil) 
+        {
+          title = [[NSProcessInfo processInfo] processName];
+        }
+        [NSApp setMainMenu: [[NSMenu allocWithZone: [NSMenu menuZone]] initWithTitle: @"temp"]];
+        menu = [[NSMenu allocWithZone: [NSMenu menuZone]] initWithTitle: @"temp"];
+        temp_item = [[NSMenuItem allocWithZone: [NSMenu menuZone]]
+                     initWithTitle: @"temp"
+                     action: NULL
+                     keyEquivalent: @""];
+        [[NSApp mainMenu] addItem: temp_item];
+        [[NSApp mainMenu] setSubmenu: menu forItem: temp_item];
+        [NSApp setAppleMenu: menu];
+        NSString *quit = [@"Quit " stringByAppendingString: title];
+        menu_item = [[NSMenuItem allocWithZone: [NSMenu menuZone]]
+                     initWithTitle: quit
+                     action: @selector(quitAction:)
+                     keyEquivalent: @"q"];
+        [menu_item setKeyEquivalentModifierMask: NSCommandKeyMask];
+        [menu_item setTarget: hiddenMembers->delegate];
+        [menu addItem: menu_item];
+      }
+      /*************************************************************/
     }
 
     void Application::finalize()
