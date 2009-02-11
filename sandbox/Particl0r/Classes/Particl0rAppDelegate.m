9d01510e8e0cd03ec2c4cd1c57a1da1fc202e616
jomanto <jomanto@gmx.net>
2009-02-11 10:45:56 +0000

    added fugly testcode for Particl0r

---
+++ sandbox/Particl0r/Classes/Particl0rAppDelegate.m
@@ -0,0 +1,40 @@
+//
+//  Particl0rAppDelegate.m
+//  Particl0r
+//
+//  Created by tboll on 10.02.09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "Particl0rAppDelegate.h"
+#import "EAGLView.h"
+
+@implementation Particl0rAppDelegate
+
+@synthesize window;
+@synthesize glView;
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    
+	glView.animationInterval = 1.0 / 60.0;
+	[glView startAnimation];
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+	glView.animationInterval = 1.0 / 5.0;
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+	glView.animationInterval = 1.0 / 60.0;
+}
+
+
+- (void)dealloc {
+	[window release];
+	[glView release];
+	[super dealloc];
+}
+
+@end
