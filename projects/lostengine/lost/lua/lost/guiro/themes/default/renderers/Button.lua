84ef11a05581634de4c4187571b526441ae6aaad
jomanto <jomanto@gmx.net>
2009-03-03 07:25:09 +0000

    added backgroundImage with properties, cleaned up controls, renderers and styles

--- projects/lostengine/lost/lua/lost/guiro/themes/default/renderers/Button.lua
+++ projects/lostengine/lost/lua/lost/guiro/themes/default/renderers/Button.lua
@@ -11,67 +11,6 @@ function Button:__init(properties) lost.guiro.Renderer.__init(self, properties)
   properties = properties or {}
 end
 
-function Button:render(canvas, button, style) lost.guiro.Renderer.render(self, canvas, button, style)
-  local globalRect = button:globalRect()
-
-  if button.fadeStates then
-    if not self.color then
-      self.color = lost.common.Color(style[button.state].color)
-    end
-    self:fade(self.color, style[button.state].color)
-    if not self.fontColor then
-      self.fontColor = lost.common.Color(style[button.state].fontColor)
-    end
-    self:fade(self.fontColor, style[button.state].fontColor)
-    if not self.lineColor then
-      self.lineColor = lost.common.Color(style[button.state].lineColor)
-    end
-    self:fade(self.lineColor, style[button.state].lineColor)
-  else
-    self.color = lost.common.Color(style[button.state].color)
-    self.fontColor = lost.common.Color(style[button.state].fontColor)
-    self.lineColor = lost.common.Color(style[button.state].lineColor)
-  end
-
-  if self.color ~= style[button.state].color or
-     self.fontColor ~= style[button.state].fontColor or
-     self.lineColor ~= style[button.state].lineColor
-  then
-    button:needsRedraw()
-  end
-
-  if self.currentImage then
-    if not button.images or not rawequal(self.currentImage, button.images[button.state]) then
-      button:removeChild(self.currentImage)
-      self.currentImage = nil
-    end
-  end
-  if button.images and button.images[button.state] then
-    if not rawequal(self.currentImage, button.images[button.state]) then
-      self.currentImage = button.images[button.state]
-    end
-    if not self.currentImage.parent then
-      button:appendChild(self.currentImage, 1)
-      self.currentImage:needsLayout()
-      self.currentImage:needsRedraw()
-    end
-    self.currentImage:color(self.color)
-  else
-    canvas:setColor(self.color)
-    canvas:drawRectFilled(globalRect)
-  end
-
-  if button.label then
-    if not button.label.parent then
-      button:appendChild(button.label)
-    end
-    if button.label:color() ~= self.fontColor then
-      button.label:color(self.fontColor)
-      button.label:needsRedraw()
-    end
-    button.label:render(canvas)
-  end
-
-  canvas:setColor(self.lineColor)
-  canvas:drawRectOutline(globalRect)
+function Button:render(canvas, button, style)
+  lost.guiro.Renderer.render(self, canvas, button, button.style[button.state])
 end
