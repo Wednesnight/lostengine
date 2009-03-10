4ea965d65e0f5cd80fb1f61b0b95418dcbdfb836
lobotony <lobotony666@yahoo.de>
2009-03-10 23:32:22 +0000

    added showMouse bindings

--- projects/lostengine/lost/lua/bindings/LostApplicationApplication.cpp
+++ projects/lostengine/lost/lua/bindings/LostApplicationApplication.cpp
@@ -49,6 +49,7 @@ namespace lost
            .def("setRunLoop", &setRunLoop)
            .def("processEvents", &Application::processEvents)
            .def("createWindow", &Application::createWindow)
+           .def("showMouse", &Application::showMouse)
            .def_readonly("loader", &Application::loader)
 //           .def_readonly("displayAttributes", &Application::displayAttributes)
 //           .def_readonly("context", &Application::context)
