ef11727fdbc7a31cb2b2fb61b295d2fcfcc66a4e
lobotony <lobotony666@yahoo.de>
2009-03-20 23:44:21 +0000

    added switches for log enabling

--- projects/lostengine/lost/common/Logger.h
+++ projects/lostengine/lost/common/Logger.h
@@ -28,9 +28,29 @@ namespace lost
 	lost::common::Logger::logMessage(m, LOGLOG_WE_DONT_WANT_TO_CONFLICT_NAMES_loc.str(), LOGLOG_WE_DONT_WANT_TO_CONFLICT_NAMES_msg.str()); \
 }
 
-#define DOUT(s) LOGLOG("DEBUG", s, LOST_COMMON_LOGGER_FILE_LINE)
-#define EOUT(s) LOGLOG("ERROR", s, LOST_COMMON_LOGGER_FILE_LINE)
-#define IOUT(s) LOGLOG("INFO", s, LOST_COMMON_LOGGER_FILE_LINE)
-#define WOUT(s) LOGLOG("WARNING", s, LOST_COMMON_LOGGER_FILE_LINE)
+
+#if defined(LOST_LOGGER_ENABLE_DOUT)
+# define DOUT(s) LOGLOG("DEBUG", s, LOST_COMMON_LOGGER_FILE_LINE)
+#else
+# define DOUT(s)
+#endif
+
+#if defined(LOST_LOGGER_ENABLE_EOUT)
+# define EOUT(s) LOGLOG("ERROR", s, LOST_COMMON_LOGGER_FILE_LINE)
+#else
+# define EOUT(s)
+#endif
+
+#if defined(LOST_LOGGER_ENABLE_IOUT)
+# define IOUT(s) LOGLOG("INFO", s, LOST_COMMON_LOGGER_FILE_LINE)
+#else
+# define IOUT(s)
+#endif
+
+#if defined(LOST_LOGGER_ENABLE_WOUT)
+# define WOUT(s) LOGLOG("WARNING", s, LOST_COMMON_LOGGER_FILE_LINE)
+#else
+# define WOUT(s)
+#endif
 
 #endif
