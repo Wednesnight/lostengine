f8689aa3c6d494be8405cf9f0d92cb05f2796490
jomanto <jomanto@gmx.net>
2009-03-25 13:41:05 +0000

    added codebase for guiro touch events

---
+++ projects/lostengine/lost/lua/lost/guiro/event/TouchEvent.lua
@@ -0,0 +1,31 @@
+module("lost.guiro.event", package.seeall)
+
+require("lost.guiro.event.Event")
+
+TouchEvent = lost.common.Class("lost.guiro.event.TouchEvent",lost.guiro.event.Event)
+
+TouchEvent.TOUCHES_BEGAN = "touchesBegan"
+TouchEvent.TOUCHES_CANCELLED = "touchesCancelled"
+TouchEvent.TOUCHES_ENDED = "touchesEnded"
+TouchEvent.TOUCHES_MOVED = "touchesMoved"
+
+local typeConvert = {}
+typeConvert[lost.application.TouchEvent.TOUCHES_BEGAN] = TouchEvent.TOUCHES_BEGAN
+typeConvert[lost.application.TouchEvent.TOUCHES_CANCELLED] = TouchEvent.TOUCHES_CANCELLED
+typeConvert[lost.application.TouchEvent.TOUCHES_ENDED] = TouchEvent.TOUCHES_ENDED
+typeConvert[lost.application.TouchEvent.TOUCHES_MOVED] = TouchEvent.TOUCHES_MOVED
+
+-- initialise it with an uncast lost.application.TouchEvent
+function TouchEvent:__init(laTouchEvent)
+  lost.guiro.event.Event.__init(self)
+
+  local lostAppTouchEvent = lost.application.TouchEvent.cast(laTouchEvent)
+
+  self.bubbles = true
+--  self.pos = lostAppTouchEvent.pos
+  self.type = typeConvert[lostAppTouchEvent.type]
+  self.size = lostAppTouchEvent:size()
+  self.touches = lostAppTouchEvent.touches
+  self.stopDispatch = false
+  self.stopPropagation = false
+end
