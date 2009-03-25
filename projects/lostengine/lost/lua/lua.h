9398c0a74b62ef8f4a62a22eaaffe54f1304129a
jomanto <jomanto@gmx.net>
2009-03-25 13:40:11 +0000

    fixed iphone build and defines

--- projects/lostengine/lost/lua/lua.h
+++ projects/lostengine/lost/lua/lua.h
@@ -15,4 +15,17 @@ extern "C" {
 #define LOST_LUA_CONST( class, name ) table[#name] = class::name;
 #define LOST_LUA_CONST_END }
 
+#undef LUA_NUMBER
+#undef LUA_NUMBER_SCAN
+#undef LUA_NUMBER_FMT
+#if TARGET_OS_IPHONE
+  #define LUA_NUMBER        float
+  #define LUA_NUMBER_SCAN		"%f"
+  #define LUA_NUMBER_FMT		"%g"
+#else
+  #define LUA_NUMBER        double
+  #define LUA_NUMBER_SCAN		"%lf"
+  #define LUA_NUMBER_FMT		"%.14g"
+#endif
+
 #endif
