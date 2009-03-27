3cc3a91547f44b066498509fba1d44f28124e83b
lobotony <lobotony666@yahoo.de>
2009-03-27 03:07:12 +0000

    added some actual drawing code

--- sandbox/Filt3rz/Filt3rz.h
+++ sandbox/Filt3rz/Filt3rz.h
@@ -5,7 +5,7 @@
 #include "lost/application/Application.h"
 #include "lost/application/KeyEvent.h"
 #include "lost/gl/FrameBuffer.h"
-
+#include "lost/camera/Camera2D.h"
 
 struct Filt3rz
 {
@@ -21,13 +21,18 @@ private:
   boost::shared_ptr<lost::gl::FrameBuffer>          framebuffer;
   boost::shared_ptr<lost::gl::Texture>              tex;
   boost::shared_ptr<lost::gl::State>                renderState;
-
-  bool contextOk;
+  boost::shared_ptr<lost::gl::State>                fboRenderState;
+  boost::shared_ptr<lost::camera::Camera2D>         fboCam;
+  boost::shared_ptr<lost::gl::Canvas>               fboCanvas;
   
   void keyHandler(boost::shared_ptr<lost::application::KeyEvent> event);
   void update(boost::shared_ptr<lost::application::Application> app);
 
   void setupFBOs();
+  void renderFbo();
+  
+  lost::math::Vec2 fboSize; // width and height of the offscreen buffer
+  lost::math::Rect fboViewport;
 };
 
 #endif
