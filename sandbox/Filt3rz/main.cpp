b220505a9a6388c650b858ea35df73540afa502a
lobotony <lobotony666@yahoo.de>
2009-04-06 18:00:49 +0000

    added sg testcode

--- sandbox/Filt3rz/main.cpp
+++ sandbox/Filt3rz/main.cpp
@@ -1,10 +1,13 @@
 #include "Filt3rz.h"
 
+void batchtest();
+
 int main(int argn, char** args)
 {
   try
   {
     static Filt3rz filt3rz;
+    batchtest();
     filt3rz.run();
   }
   catch (std::exception& e)
