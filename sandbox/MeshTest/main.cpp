7b6079a4708b3ecee4a4cdbe7b293556e7b52bdc
lobotony <lobotony666@yahoo.de>
2009-04-08 09:47:06 +0000

    added application instance and window

--- sandbox/MeshTest/main.cpp
+++ sandbox/MeshTest/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <stdexcept>
 #include "lost/common/Logger.h"
+#include "lost/application/Application.h"
+#include "lost/application/KeyEvent.h"
+#include "lost/event/Receive.h"
 
 using namespace std;
+using namespace lost;
+using namespace lost::math;
+using namespace lost::event;
+using namespace lost::application;
+
+ApplicationPtr app;
+WindowPtr window;
+
+void update(ApplicationPtr app)
+{
+    DOUT("update");
+}
+
+
+void keyHandler(KeyEventPtr event)
+{
+}
 
 int main(int argn, char** args)
 {
   try
   {
     DOUT("asd");
-/*    static Filt3rz filt3rz;
-    batchtest();
-    filt3rz.run();*/
+  app = Application::create(update);
+//  app->runLoopWaitsForEvents(true);
+  app->addEventListener(lost::application::KeyEvent::KEY_DOWN(), receive<KeyEvent>(keyHandler));
+  window = app->createWindow("window", WindowParams("Filt3rz", Rect(50,200,640,480)));
+  window->context->makeCurrent();
+  app->run();      
   }
   catch (std::exception& e)
   {
