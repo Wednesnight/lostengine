ca163fe3e0d1f20dbfab5d11568f9e0cf501d99d
lobotony <lobotony666@yahoo.de>
2009-04-09 00:05:46 +0000

    first rudimentary working version of mesh and line

--- projects/lostengine/lost/mesh/Line.h
+++ projects/lostengine/lost/mesh/Line.h
@@ -1,4 +1,5 @@
-#ifndef LOST_MESH_LINE
+
+  #ifndef LOST_MESH_LINE
 #define LOST_MESH_LINE
 
 #include "lost/mesh/Mesh.h"
@@ -14,19 +15,41 @@ struct Line : public MESHTYPE
   typedef typename MESHTYPE::VertexType VertexType;
 
   VertexType vertices[2];
+  uint32_t    dataSize;
+  void init()
+  {
+    MESHTYPE::vertices(true); // create vertex array
+    MESHTYPE::indices(true);  // create index array
+    MESHTYPE::vertexBuffer->bindBufferData(NULL, 2); // create empty buffer    
+    typename MESHTYPE::IndexType idx[2];
+    idx[0] = 0;
+    idx[1] = 1;
+    MESHTYPE::indexBuffer->bindBufferData(idx, 2);
+    dataSize = sizeof(vertices);
+    MESHTYPE::drawMode = GL_LINE_STRIP;
+  }
 
   Line(const VertexType& start, const VertexType& end)
   {
-    
+    init();
+    update(start, end);
+  }
+  
+  Line()
+  {
+    init();
   }
   
   virtual ~Line()
   {
   }
   
-  void start(const VertexType& inStart) {vertices[0] = inStart;}
-  VertexType start() { return vertices[0];}
-  
+  void update(const VertexType& inStart, const VertexType& inEnd)
+  {
+    vertices[0]=inStart;
+    vertices[1] = inEnd;
+    MESHTYPE::vertexBuffer->bindBufferSubData(0,dataSize, vertices);
+  }
 };
 
 typedef Line<Mesh2D> Line2D;
--- projects/lostengine/lost/mesh/Mesh.h
+++ projects/lostengine/lost/mesh/Mesh.h
@@ -6,6 +6,8 @@
 #include "lost/gl/ElementArrayBuffer.h"
 #include "lost/common/Logger.h"
 #include "lost/gl/Context.h"
+#include "lost/gl/State.h"
+#include "lost/gl/StateParam.h"
 
 namespace lost
 {
@@ -16,7 +18,7 @@ namespace mesh
 // and issues the gl calls for drawing
 struct Mesh
 {
-  virtual void render(gl::ContextPtr ctx) = 0;
+  virtual void draw(gl::ContextPtr ctx) = 0;
 };
 
 typedef boost::shared_ptr<Mesh> MeshPtr;
@@ -31,29 +33,61 @@ template<
   typename TCT> // texcoordtype
 struct BufferedMesh : public Mesh
 {
-  boost::shared_ptr<gl::ElementArrayBuffer<IT> >  indices; 
-  boost::shared_ptr<gl::ArrayBuffer<VT> >         vertices;
-  boost::shared_ptr<gl::ArrayBuffer<NT> >         normals;
-  boost::shared_ptr<gl::ArrayBuffer<CT> >         colors;
-  boost::shared_ptr<gl::ArrayBuffer<TCT> >        texcoords;
+  boost::shared_ptr<gl::ElementArrayBuffer<IT> >  indexBuffer; 
+  boost::shared_ptr<gl::ArrayBuffer<VT> >         vertexBuffer;
+  boost::shared_ptr<gl::ArrayBuffer<NT> >         normalBuffer;
+  boost::shared_ptr<gl::ArrayBuffer<CT> >         colorBuffer;
+  boost::shared_ptr<gl::ArrayBuffer<TCT> >        texCoordBuffer;
   
   typedef IT IndexType;
   typedef VT VertexType;
   typedef NT NormalType;
   typedef CT ColorType;
   typedef TCT TexCoordType;
+  
+  typedef gl::ElementArrayBuffer<IT>  IndexBufferType;
+  typedef gl::ArrayBuffer<VT>         VertexBufferType;
+  typedef gl::ArrayBuffer<NT>         NormalBufferType;
+  typedef gl::ArrayBuffer<CT>         ColorBufferType;
+  typedef gl::ArrayBuffer<TCT>        TexCoordBufferType;
+
+  gl::StateParamPtr  vertexParam;
+  gl::StateParamPtr  normalParam;
+  gl::StateParamPtr  colorParam;
+  gl::StateParamPtr  texCoordParam;
+  gl::StatePtr  meshState;
+
+  GLenum drawMode; // GL_LINES, GL_TRIANGLES etc.
       
   BufferedMesh()
   {
+    vertexParam = gl::VertexArray::create(false);
+    normalParam = gl::NormalArray::create(false);
+    colorParam = gl::ColorArray::create(false);
+    texCoordParam = gl::TextureArray::create(false);
+    meshState = gl::State::create(vertexParam, normalParam, colorParam, texCoordParam);
   }
   
   virtual ~BufferedMesh()
   {
   }
   
-  virtual void render(gl::ContextPtr ctx)
+  void indices(bool v) { if(v) indexBuffer.reset(new IndexBufferType); else indexBuffer.reset(); }
+  void vertices(bool v) { if(v) vertexBuffer.reset(new VertexBufferType); else vertexBuffer.reset(); vertexParam->enable=v; }
+  void normals(bool v) { if(v) normalBuffer.reset(new NormalBufferType); else normalBuffer.reset(); normalParam->enable=v;}
+  void colors(bool v) { if(v) colorBuffer.reset(new ColorBufferType); else colorBuffer.reset(); colorParam->enable=v; }
+  void texCoords(bool v) { if(v) texCoordBuffer.reset(new TexCoordBufferType); else texCoordBuffer.reset(); texCoordParam->enable=v;}
+  
+  virtual void draw(gl::ContextPtr ctx)
   {
-    DOUT("render mesh");
+    ctx->pushState(meshState);
+    vertexBuffer->bindVertexPointer();
+    indexBuffer->bind();
+    if(normalBuffer) normalBuffer->bindNormalPointer();
+    if(colorBuffer) colorBuffer->bindColorPointer();
+    if(texCoordBuffer) texCoordBuffer->bindTexCoordPointer();
+    indexBuffer->drawElements(drawMode);
+    ctx->popState();
   }
   
 };
