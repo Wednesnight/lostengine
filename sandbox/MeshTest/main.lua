0d11eec4ac7c96d49d564fdf1b1b9411419e9e69
lobotony <lobotony666@yahoo.de>
2009-04-21 02:46:01 +0000

    moved shader init code to lua

--- sandbox/MeshTest/main.lua
+++ sandbox/MeshTest/main.lua
@@ -1,5 +1,19 @@
+local Color = lost.common.Color
+
+-- requires camera3D to be set from C++
 function initShaders(loader)
-  log.debug("initShaders")
-  shader = lost.gl.loadShader(loader, "light")
+  local shader = lost.gl.loadShader(loader, "light")
+  shader:enable()
+  shader:validate()
+  if not shader:validated() then
+    log.debug("Problem found during validation: \n"..lightShader:log())
+  else
+    log.debug("Program validated OK")
+  end
+  shader:set("LightPosition", camera3D:position());
+  shader:set("LightDiffuse", Color(1, 0.5, 0.5));
+  shader:set("LightAmbient", Color(1, 0.3, 0.3));
+  shader:set("LightSpecular", Color(1, 0.1, 0.1));  
+  shader:disable()
   return shader
 end
