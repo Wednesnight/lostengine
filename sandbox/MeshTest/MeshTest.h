c9c6e78b384b5ae51ed2ee9412dca2a228a09601
lobotony <lobotony666@yahoo.de>
2009-04-24 01:59:02 +0000

    testing, updated to new context in window, YAY

--- sandbox/MeshTest/MeshTest.h
+++ sandbox/MeshTest/MeshTest.h
@@ -7,7 +7,6 @@
 #include "lost/mesh/Quad.h"
 #include "lost/camera/Camera3D.h"
 #include "lost/camera/Camera2D.h"
-#include "lost/gl/XContext.h"
 #include "lost/gl/ShaderHelper.h"
 
 struct MeshTest : public lost::application::UiTasklet
@@ -22,11 +21,11 @@ struct MeshTest : public lost::application::UiTasklet
 
   void keyHandler(lost::application::KeyEventPtr event);
 
-  lost::gl::XContextPtr ctx;
-
   lost::mesh::Line2DPtr         line;
   lost::mesh::Mesh3DPtr         cube;
   lost::mesh::Quad2DPtr         quad;
+  lost::mesh::Quad2DPtr         quad2;
+  lost::mesh::Quad2DPtr         quad3;
   lost::camera::CameraPtr       camera2D;
   lost::camera::Camera3DPtr     camera3D;
   
