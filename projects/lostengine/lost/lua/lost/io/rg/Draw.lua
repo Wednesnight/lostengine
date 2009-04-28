89bd8984553c6b14991ce243fc896d405a611386
lobotony <lobotony666@yahoo.de>
2009-04-28 08:00:58 +0000

    added first try for io.rg

---
+++ projects/lostengine/lost/lua/lost/io/rg/Draw.lua
@@ -0,0 +1,23 @@
+module("lost.io", package.seeall)
+
+
+require("lost.io.Loader")
+
+-- mesh = (any meshPtr, will be cast to MeshPtr) (mandatory)
+-- material
+function Loader:Draw(definition)
+  local result = lost.rg.Draw.create(definition["mesh"])
+  
+  if definition["modelTransform"] then
+    result.modelTransform = definition["modelTransform"]
+  end
+  
+  if definition["material"] then
+    local mat = definition["material"]
+    for k,v in mat do
+      result.material[k] = v
+    end
+  end
+  
+  return result
+end
