8841cb31bdefc30c655e304b6aad835efa76f55d
lobotony <lobotony666@yahoo.de>
2009-05-02 18:39:09 +0000

    added new nodes needed for Filt3rz

---
+++ projects/lostengine/lost/rg/FrameBuffer.cpp
@@ -0,0 +1,26 @@
+#include "lost/rg/FrameBuffer.h"
+#include "lost/gl/Context.h"
+
+namespace lost
+{
+namespace rg
+{
+
+FrameBuffer::FrameBuffer(gl::FrameBufferPtr inFb)
+{
+  fb = inFb;
+}
+
+NodePtr FrameBuffer::create(gl::FrameBufferPtr inFb)
+{
+  return FrameBufferPtr(new FrameBuffer(inFb));
+}
+
+void FrameBuffer::process(gl::ContextPtr ctx)
+{
+  if(!active) return;
+  ctx->frameBuffer(fb);
+}
+
+}
+}
