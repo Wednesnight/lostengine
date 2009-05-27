bd6e1ff41bdb8f4f45887186631b7ff953d3e32e
lobotony <lobotony666@yahoo.de>
2009-05-27 15:09:20 +0000

    cleaned up, removed dead code

--- sandbox/MeshTest/main.lua
+++ sandbox/MeshTest/main.lua
@@ -1,76 +1,7 @@
 require("lost.io.Rg")
 
-local Color = lost.common.Color
-local Quad2D = lost.mesh.Quad2D
-local Line2D = lost.mesh.Line2D
-local Material = lost.mesh.Material
-local Vec2 = lost.math.Vec2
-local Vec3 = lost.math.Vec3
-local Rect = lost.math.Rect
-local MatrixTranslation = lost.math.MatrixTranslation
-local Camera2D = lost.camera.Camera2D
-local Camera3D = lost.camera.Camera3D
-local rg = lost.rg;
-
-
 function startup(tasklet)
---[[
-  -- cam 2D
-  camera2D = Camera2D.create(Rect(0, 0, 640, 480))
-
-  -- cam 3D
-  camera3D = Camera3D.create(Rect(0, 0, 640, 480))
-  camera3D:fovY(45.0)
-  camera3D:depth(Vec2(1.0, 1000.0))
-  camera3D:position(Vec3(1,2,2))
-  camera3D:target(Vec3(0,0,0))
-  camera3D:stickToTarget(true)  
-
-  local shader = lost.gl.loadShader(tasklet.loader, "light")
-  shader:enable()
-  shader:validate()
-  if not shader:validated() then
-    log.debug("Problem found during validation: \n"..shader:log())
-  else
-    log.debug("Program validated OK")
-  end
-  shader:set("LightPosition", camera3D:position());
-  shader:set("LightDiffuse",  Color(1.0, 1.0, 1.0));
-  shader:set("LightAmbient",  Color(1.0, 1.0, 1.0));
-  shader:set("LightSpecular", Color(1.0, 1.0, 1.0));  
-  shader:disable()
-  
-  -- these are deliberately global, so the C++ part can fetch the easier from the interpreter
-  quad = Quad2D(tasklet.loader:load("zim.png"), true)
-  quad:updateSize(Vec2(640, 480), true)
-  quad2 = Quad2D(Rect(0,0,3,3))
-  quad2.material.color = Color(1,0,0);
-  quad3 = Quad2D(tasklet.loader:load("zim.png"), true);
-  quad3.modelTransform = MatrixTranslation(Vec3(400, 370, 0))
-  
-  line = Line2D(Vec2(0,0), Vec2(639,479))
-  line.material.color = Color(1,1,0)
-  log.debug("loading file")
-  filedata = tasklet.loader:load("cube_tri.obj")
-  log.debug("parsing file")
---  cube = lost.mesh.Loader.obj(filedata);
---  cube.material.color = Color(0,1,0)
---  cube.material.shader = shader
-
-  scene = rg.Node.create()
-  scene:add(rg.Camera.create(camera2D));
-  scene:add(rg.DepthTest.create(false));
-  scene:add(rg.Draw.create(quad));
-  scene:add(rg.Draw.create(quad2));
-  scene:add(rg.Draw.create(quad3));
-  scene:add(rg.Draw.create(line));
-  scene:add(rg.Draw.create(meter));
-  scene:add(rg.Camera.create(camera3D));
-  scene:add(rg.DepthTest.create(true));
---  scene:add(rg.Draw.create(cube));
-]]--
   local l = lost.io.Loader(tasklet.loader)
-  local scene2 = l:loadScene("testscene.lua")
-  scene = scene2
+  scene = l:loadScene("testscene.lua")
   return true
 end
