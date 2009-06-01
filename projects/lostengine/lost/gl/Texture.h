482ec202a5c00df51074fc7a09a54ce00079146e
lobotony <lobotony666@yahoo.de>
2009-06-01 16:11:17 +0000

    moved texture coordinate generation functions from Quad to Texture, cleaned up, simplified Quad code

--- projects/lostengine/lost/gl/Texture.h
+++ projects/lostengine/lost/gl/Texture.h
@@ -86,6 +86,58 @@ namespace lost
       boost::uint32_t height;      
       boost::uint32_t dataWidth; 
       boost::uint32_t dataHeight;
+
+
+      // texture coordinate helpers
+      // returns a normalised texture coordinate for the given pixel X coordinate in the range [0,1]
+      // the texcoord is correctly offset so that the coord is centered on a texel for pixel perfect 2D drawing.
+      inline float normalisedXCoord(boost::uint32_t pixelXCoord)
+      {
+        float result = 0.0f;
+        if(width > 0)
+        {
+          result = (((float)pixelXCoord)+.5f)/((float)width);
+        }
+        
+        return result;
+      }
+
+      // returns a normalised texture coordinate for the given pixel Y coordinate in the range [0,1]
+      // the texcoord is correctly offset so that the coord is centered on a texel for pixel perfect 2D drawing.
+      inline float normalisedYCoord(boost::uint32_t pixelYCoord)
+      {
+        float result = 0.0f;
+        if(height > 0)
+        {
+          result = (((float)pixelYCoord)+.5f)/((float)height);
+        }
+        
+        return result;
+      }
+
+      inline math::Vec2 normalisedCoord(const math::Vec2 pixelCoord)
+      {
+        return math::Vec2(normalisedXCoord(pixelCoord.x), normalisedYCoord(pixelCoord.y));
+      }
+      
+      // returns pixel 0,0 as normalised textur coordianate 
+      inline math::Vec2 bottomLeftTexCoord()
+      {
+        return normalisedCoord(math::Vec2(0,0));
+      }
+      
+      // returns the top right bitmap pixel as normalised textur coordinate
+      // this funcrtion does take into account that the ctua texture size can be larger than 
+      // the uploaded data, so you can be sure to receive only the topright most bitmap pixel
+      inline math::Vec2 topRightTexCoord()
+      {
+        boost::uint32_t maxX = std::min(dataWidth, width);
+        boost::uint32_t maxY = std::min(dataHeight, height);
+        float fMaxX = maxX ? (float)(maxX-1) : (float)maxX;
+        float fMaxY = maxY ? (float)(maxY-1) : (float)maxY;
+        return normalisedCoord(math::Vec2(fMaxX, fMaxY));
+      }
+      
     private:
       void create();
     };
