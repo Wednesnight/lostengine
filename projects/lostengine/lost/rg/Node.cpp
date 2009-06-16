eb9845c624b4ebd848ee10289dee678721485285
jomanto <jomanto@gmx.net>
2009-06-16 12:42:53 +0000

    integrated rg Nodes into guiro View hierarchy

--- projects/lostengine/lost/rg/Node.cpp
+++ projects/lostengine/lost/rg/Node.cpp
@@ -3,60 +3,65 @@
 
 namespace lost
 {
-namespace rg
-{
-
-Node::Node()
-{
-  name = "node"; 
-  active = true;
-}
-
-Node::~Node()
-{
-  
-}
-
-void Node::add(NodePtr child)
-{
-  children.push_back(child);
-}
-
-void Node::process(gl::ContextPtr ctx)
-{
-  if(!active) return; 
-  
-  for(std::list<NodePtr>::iterator i=children.begin(); i!=children.end(); ++i)
+  namespace rg
   {
-    if((*i)->active)
-      (*i)->process(ctx);
-  }
-}
 
-NodePtr Node::create()
-{
-  return NodePtr(new Node);
-}
-
-NodePtr Node::recursiveFindByName(const std::string& inName)
-{
-  NodePtr result;
-  for(std::list<NodePtr>::iterator i=children.begin();
-      i!=children.end();
-      ++i)
-  {
-    if((*i)->name == inName)
+    Node::Node()
     {
-      result = *i;
-      break;
+      name = "node"; 
+      active = true;
     }
-    else if(result = (*i)->recursiveFindByName(inName))
-    {
-      break;
-    }
-  }
-  return result;
-}
 
-}
+    Node::~Node()
+    {
+      
+    }
+
+    void Node::add(NodePtr child)
+    {
+      children.push_back(child);
+    }
+
+    void Node::remove(NodePtr child)
+    {
+      children.remove(child);
+    }
+      
+      void Node::process(gl::ContextPtr ctx)
+    {
+      if(!active) return; 
+      
+      for(std::list<NodePtr>::iterator i=children.begin(); i!=children.end(); ++i)
+      {
+        if((*i)->active)
+          (*i)->process(ctx);
+      }
+    }
+
+    NodePtr Node::create()
+    {
+      return NodePtr(new Node);
+    }
+
+    NodePtr Node::recursiveFindByName(const std::string& inName)
+    {
+      NodePtr result;
+      for(std::list<NodePtr>::iterator i=children.begin();
+          i!=children.end();
+          ++i)
+      {
+        if((*i)->name == inName)
+        {
+          result = *i;
+          break;
+        }
+        else if(result = (*i)->recursiveFindByName(inName))
+        {
+          break;
+        }
+      }
+      return result;
+    }
+
+  }
 }
