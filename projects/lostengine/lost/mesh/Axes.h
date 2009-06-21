f45a4c3224070ee2df73f408deea0bd72bd2f66a
lobotony <lobotony666@yahoo.de>
2009-06-21 21:38:16 +0000

    #104 add AxesMesh that should work, but doesn't show up

--- projects/lostengine/lost/mesh/Axes.h
+++ projects/lostengine/lost/mesh/Axes.h
@@ -2,18 +2,23 @@
 #define LOST_MESH_AXES_H
 
 #include "lost/mesh/Mesh.h"
-#include "lost/math/Axes.h"
 
 namespace lost
 {
 namespace mesh
 {
 
+
+// length of the axes is currently hard coded to 1
+// origin is always 0,0,0, use the transform to change that
 template<typename MESHTYPE>
 struct Axes : public MESHTYPE
 {
+  typedef typename MESHTYPE::VertexType VertexType;
+
   Axes()
   {
+    float len = 1;
     this->drawMode = GL_LINES;
     boost::uint32_t numLines = 6;
     boost::uint32_t numVertices = numLines*2;
@@ -22,12 +27,66 @@ struct Axes : public MESHTYPE
     this->indices(true);
     this->vertices(true);
     this->colors(true);
+    this->resetIndices(numIndices);
+    this->resetVertices(numVertices);
+    this->resetColors(numColors);
     
-    // positive 
+    // X red positive
+    this->vertexData[0] = VertexType(0,0,0);
+    this->colorData[0] = common::Color(1,0,0,1);
+    this->indexData[0] = 0;
+    this->vertexData[1] = VertexType(len,0,0);
+    this->colorData[1] = common::Color(1,0,0,1);
+    this->indexData[1] = 1;
+
+    // X red negative alpha
+    this->vertexData[2] = VertexType(0,0,0);
+    this->colorData[2] = common::Color(1,0,0,.5f);
+    this->indexData[2] = 2;
+    this->vertexData[3] = VertexType(-len,0,0);
+    this->colorData[3] = common::Color(1,0,0,.5f);
+    this->indexData[3] = 3;
+    
+    // Y green positive
+    this->vertexData[4] = VertexType(0,0,0);
+    this->colorData[4] = common::Color(0,1,0,1);
+    this->indexData[4] = 4;
+    this->vertexData[5] = VertexType(0,len,0);
+    this->colorData[5] = common::Color(0,1,0,1);
+    this->indexData[5] = 5;
+
+    // Y green negative
+    this->vertexData[6] = VertexType(0,0,0);
+    this->colorData[6] = common::Color(0,1,0,.5f);
+    this->indexData[6] = 6;
+    this->vertexData[7] = VertexType(0,-len,0);
+    this->colorData[7] = common::Color(0,1,0,.5f);
+    this->indexData[7] = 7;
+
+    // Z blue positive
+    this->vertexData[8] = VertexType(0,0,0);
+    this->colorData[8] = common::Color(0,0,1,1);
+    this->indexData[8] = 8;
+    this->vertexData[9] = VertexType(0,0,len);
+    this->colorData[9] = common::Color(0,0,1,1);
+    this->indexData[9] = 9;
+    
+    // Z blue negative
+    this->vertexData[10] = VertexType(0,0,0);
+    this->colorData[10] = common::Color(0,0,1,.5f);
+    this->indexData[10] = 10;
+    this->vertexData[11] = VertexType(0,0,-len);
+    this->colorData[11] = common::Color(0,0,1,.5f );
+    this->indexData[11] = 11;
     
     this->transferVertices();
     this->transferIndices();
     this->transferColors();
+  }
+  
+  static lost::shared_ptr<Axes<MESHTYPE> > create()
+  {
+    return lost::shared_ptr<Axes<MESHTYPE> >(new Axes);
   }
   
 };
