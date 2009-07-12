084c0d0457cfd071bcf39d471bf0a94f796e9230
lobotony <lobotony666@yahoo.de>
2009-07-12 19:56:55 +0000

    kinda fixed scalegrid, drawing is still off by a pixel, might be because of general 2d inaccuracies

--- projects/lostengine/lost/mesh/ScaleGrid.h
+++ projects/lostengine/lost/mesh/ScaleGrid.h
@@ -14,7 +14,7 @@ namespace mesh
  * The l/r/t/b measurements should be within the range of the raw pixel data of the texture to
  * achieve meaningful results. Everything else is undefined and will probably look messed up.
  * The specified rect is treated as pixel coordinates in a 2d coordinate system.
- * l,r,t,b are absolue pixel coordinates in the bitmap pixel coord space.
+ * l,r,t,b are absolue pixel coordinates in the bitmap pixel coord space, zero based with origin at lower left.
  * 0,0 is the bottom left corner.
  */
 template<typename MESHTYPE>
@@ -62,33 +62,53 @@ struct ScaleGrid : public MESHTYPE
     // 0,3,12,15 are the corners of the provided rect
     // all others are derived from the l/r/t/b measurement that were provided.
     
+    float dx = tex->dataMaxX() - right;
+    float dy = tex->dataMaxY() - top;
+    
     // first, we calculate the vertex coordinates relative to 0,0 since these are the pixel coordinates 
     // we need to calculate the texel coordinates from the texture.
     math::Rect zr(0,0,rect.width, rect.height); // zero based rect for the first pass of the calculations
     this->vertexData[0] = VertexType(zr.topLeft());
     this->vertexData[1] = VertexType(left, zr.top());
-    this->vertexData[2] = VertexType(right, zr.top());
+    this->vertexData[2] = VertexType(zr.right()-dx, zr.top());
     this->vertexData[3] = VertexType(zr.topRight());
-    this->vertexData[4] = VertexType(zr.left(), top);
-    this->vertexData[5] = VertexType(left, top);
-    this->vertexData[6] = VertexType(right, top);
-    this->vertexData[7] = VertexType(zr.right(), top);
+    this->vertexData[4] = VertexType(zr.left(), zr.top()-dy);
+    this->vertexData[5] = VertexType(left, zr.top()-dy);
+    this->vertexData[6] = VertexType(zr.right()-dx, zr.top()-dy);
+    this->vertexData[7] = VertexType(zr.right(), zr.top()-dy);
     this->vertexData[8] = VertexType(zr.left(), bottom);
     this->vertexData[9] = VertexType(left, bottom);
-    this->vertexData[10] = VertexType(right, bottom);
+    this->vertexData[10] = VertexType(zr.right()-dx, bottom);
     this->vertexData[11] = VertexType(zr.right(), bottom);
     this->vertexData[12] = VertexType(zr.bottomLeft());
     this->vertexData[13] = VertexType(left, zr.bottom());
-    this->vertexData[14] = VertexType(right, zr.bottom());
+    this->vertexData[14] = VertexType(zr.right()-dx, zr.bottom());
     this->vertexData[15] = VertexType(zr.bottomRight());
 
-    // now we can use these pixel coordinates as coordinates into the provided texture
-    // and use the texture to calculate normalised texture coordinates for the actual mapping
-    for(uint32_t i=0; i<numVertices; ++i)
-    {
-      this->texCoordData[i] = tex->normalisedCoord(this->vertexData[i]);
-    }
 
+    float mx = tex->dataMaxX();
+    float my = tex->dataMaxY();
+
+    this->texCoordData[0] = tex->normalisedCoord(VertexType(0,my));
+    this->texCoordData[1] = tex->normalisedCoord(VertexType(left, my));
+    this->texCoordData[2] = tex->normalisedCoord(VertexType(right, my));
+    this->texCoordData[3] = tex->normalisedCoord(VertexType(mx, my));
+    
+    this->texCoordData[4] = tex->normalisedCoord(VertexType(0,top));
+    this->texCoordData[5] = tex->normalisedCoord(VertexType(left,top));
+    this->texCoordData[6] = tex->normalisedCoord(VertexType(right, top));
+    this->texCoordData[7] = tex->normalisedCoord(VertexType(mx, top));
+    
+    this->texCoordData[8] = tex->normalisedCoord(VertexType(0,bottom));
+    this->texCoordData[9] = tex->normalisedCoord(VertexType(left,bottom));
+    this->texCoordData[10] = tex->normalisedCoord(VertexType(right, bottom));
+    this->texCoordData[11] = tex->normalisedCoord(VertexType(mx, bottom));
+    
+    this->texCoordData[12] = tex->normalisedCoord(VertexType(0,0));
+    this->texCoordData[13] = tex->normalisedCoord(VertexType(left,0));
+    this->texCoordData[14] = tex->normalisedCoord(VertexType(right,0));
+    this->texCoordData[15] = tex->normalisedCoord(VertexType(mx, 0));
+    
     // and since the input rect defines the geometry and we built it zero based up to here,
     // we need to add the origin of the input rect to all previously calculated vertices
     // to achieve the final geometry
@@ -105,6 +125,13 @@ struct ScaleGrid : public MESHTYPE
     // top left starts with top left corner of quad,
     // bottom right starts with bottom left corner of the quad
     // all tris counterclockwise
+/*    this->indexData[0] = 0;
+    this->indexData[1] = 4;
+    this->indexData[2] = 1;
+    this->indexData[3] = 4;
+    this->indexData[4] = 5;
+    this->indexData[5] = 1;*/
+        
     this->indexData[0] = 0;
     this->indexData[1] = 4;
     this->indexData[2] = 1;
