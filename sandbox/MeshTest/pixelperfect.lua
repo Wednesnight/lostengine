b502bd4a8a0a919c6cf0b826f07d7ad070009162
lobotony <lobotony666@yahoo.de>
2009-07-16 12:35:40 +0000

    added test scene for pixel perfect rendering alignment

---
+++ sandbox/MeshTest/pixelperfect.lua
@@ -0,0 +1,38 @@
+local Rect = lost.math.Rect
+local Vec2 = lost.math.Vec2
+local Vec3 = lost.math.Vec3
+local Color = lost.common.Color
+local MatrixTranslation = lost.math.MatrixTranslation
+
+local zimtex = lostgl:Texture
+{
+  filename = "zim.png"
+}
+
+
+lostrg:Scene
+{
+	name = "rootNode",
+  lostrg:ClearColor{ color = Color(0,0,0,1) },
+  lostrg:Clear{mask = gl.GL_COLOR_BUFFER_BIT + gl.GL_DEPTH_BUFFER_BIT},
+	lostrg:Camera2D
+  {
+    name = "2D Cam",
+    viewport = Rect(0,0,screenSize.x,screenSize.y)
+  },
+  lostrg:DepthTest
+  {
+    false
+  },
+  lostrg:Draw
+  {
+    name = "little zim",
+    active = true,
+    mesh = lostmesh:Quad2D
+    {
+      texture = zimtex,
+      flip = true,
+      transform = MatrixTranslation(Vec3(400, 370, 0))
+    }
+  }  
+}
