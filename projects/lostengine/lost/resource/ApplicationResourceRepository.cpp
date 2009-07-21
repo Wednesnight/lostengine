fd161c41a529df55687d382efdd67f2876918df2
lobotony <lobotony666@yahoo.de>
2009-07-21 19:51:11 +0000

    fixed glGet for clearColor in context, added material evaluation to declarative Text constructor, cleaned up resource loading, added Helper

--- projects/lostengine/lost/resource/ApplicationResourceRepository.cpp
+++ projects/lostengine/lost/resource/ApplicationResourceRepository.cpp
@@ -1,6 +1,7 @@
 #include "lost/resource/ApplicationResourceRepository.h"
 #include "lost/platform/Platform.h"
 #include "lost/common/Logger.h"
+#include "lost/resource/Helper.h"
 #include <fstream>
 
 namespace lost
@@ -16,23 +17,10 @@ ApplicationResourceRepository::~ApplicationResourceRepository() {}
 
 lost::shared_ptr<lost::resource::File> ApplicationResourceRepository::load( const boost::filesystem::path& relativePath)
 {
-  lost::shared_ptr<lost::resource::File> result(new lost::resource::File);
   boost::filesystem::path absolutePath = applicationResourcePath / relativePath;
-  std::string path = absolutePath.string();
+  FilePtr result = loadFromAbsolutePath(absolutePath.string());
   result->location = relativePath.string();
-
-  std::ifstream ifs( path.c_str(), std::ios_base::in | std::ios_base::binary );
-
-  if(!ifs.good()) { throw std::runtime_error( "couldn't open file: '"+ path +"'" ); }
-
-  ifs.seekg(0,std::ios_base::end);
-  if(ifs.fail()) throw std::runtime_error("failed");
-  unsigned long filesize = ifs.tellg();
-  ifs.seekg(std::ios_base::beg);
-  result->size = filesize;
-  result->data.reset(new char[filesize]);
-  ifs.read(result->data.get(), filesize);
-
+  
   return result;
 }
 }
