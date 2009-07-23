6da85b7577e78e0a87cf19d3713b2658dd64ff21
lobotony <lobotony666@yahoo.de>
2009-07-23 12:44:30 +0000

    trying to add spinning cube as a demo

--- projects/Playground/Controller.h
+++ projects/Playground/Controller.h
@@ -4,14 +4,7 @@
 #include "lost/application/Tasklet.h"
 #include "lost/application/KeyEvent.h"
 #include "lost/application/DropEvent.h"
-#include "lost/mesh/Line.h"
-#include "lost/mesh/Quad.h"
-#include "lost/camera/Camera3D.h"
-#include "lost/camera/Camera2D.h"
-#include "lost/gl/ShaderHelper.h"
 #include "lost/rg/Node.h"
-#include "lost/meter/Meter.h"
-#include "lost/font/TrueTypeFont.h"
 #include "lost/application/Application.h"
 
 struct Controller : public lost::application::Tasklet
--- projects/Playground/fuxx0r/main.lua
+++ projects/Playground/fuxx0r/main.lua
@@ -1,16 +1,29 @@
+require("lost.io.Rg")
+
+
 hasWindow = true
 windowParams = lost.application.WindowParams("fuxx0r", lost.math.Rect(50,200,640,480))
 
-local node = lost.rg.Node.create()
+angle = 0
+lastUpdate = 0
 
 function startup(tasklet)
   log.debug("startup")
+	local l = lost.io.Loader(tasklet.loader)
+  threedScene = l:loadScene("3dScene.lua")
+  local drawCube = lost.rg.Draw.cast(threedScene:recursiveFindByName("cube"))
+  cubeMesh = drawCube.mesh
+	lastUpdate = lost.platform.currentTimeMilliSeconds()
   return true
 end
 
 function update(tasklet)
---  log.debug("update")
-  node:process(tasklet.window.context)
+	local currentTime = lost.platform.currentTimeMilliSeconds() 
+	local dt = currentTime - lastUpdate
+  angle = math.fmod(dt*50+angle, 360);
+  cubeMesh.modelTransform = lost.math.MatrixRotX(angle) * lost.math.MatrixRotY(angle);
+  threedScene:process(tasklet.window.context)
+	tasklet.window.context:swapBuffers()
   return true
 end
 
