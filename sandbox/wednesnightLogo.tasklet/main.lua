5845cb195648f8ba8d968ad620e867b05e95d7bf
lobotony <lobotony666@yahoo.de>
2009-07-26 23:39:03 +0000

    restructured: now uses Animator and Entity classes

--- sandbox/wednesnightLogo.tasklet/main.lua
+++ sandbox/wednesnightLogo.tasklet/main.lua
@@ -1,4 +1,7 @@
 require("lost.declarative.Context")
+require("Animator")
+require("LogoEntity")
+require("Fullscreen2DEntity")
 
 local WindowParams = lost.application.WindowParams
 local Rect = lost.math.Rect
@@ -12,88 +15,25 @@ hasWindow = true
 windowRect = Rect(200,200,640,480)
 windowParams = WindowParams("logo test", windowRect)
 
-backgroundColor = Color(0,0,0,1)
-
-local angle = 13
-local startTime = 0
-local lastUpdate = 0
-
-local logoTranslationMatrix = MatrixTranslation(Vec3(200,200,0))
-
 local exportScreens = false
 local running = true
 
 function startup(tasklet)
   dcl = lost.declarative.Context(tasklet.loader)
-
-  titleFont = dcl.font:TrueTypeFont
-  {
-    filename = "miserable.ttf",
-    atlasSize = Vec2(512,512)
-  }
-  
-  renderedText = dcl.font:Text
-  {
-    text = "Wednesnight",
-    size = 60,
-    font = titleFont,
-    transform = MatrixTranslation(Vec3(50,50,0))
-  }
-  
-  rootNode = dcl.rg:Node
-  {
-    dcl.rg:ClearColor{color = backgroundColor},
-    dcl.rg:Clear{mask = gl.GL_COLOR_BUFFER_BIT + gl.GL_DEPTH_BUFFER_BIT},
-    dcl.rg:Camera2D
-    {
-      viewport = Rect(0,0,windowRect.width,windowRect.height)
-    },
-    dcl.rg:DepthTest
-    {
-      false
-    },
-    dcl.rg:Draw
-    {
-      mesh = renderedText
-    } 
-  }
-  
-  lastUpdate = lost.platform.currentTimeMilliSeconds()
-  startTime = lastUpdate
+  animator = Animator()
+  logoEntity = LogoEntity()
+  rootEntity = Fullscreen2DEntity(Color(0,0,0,1))
+  rootEntity:add(logoEntity)
   return true
 end
 
-function doUpdate(tasklet, dt)
-  rootNode:process(tasklet.window.context)
-  angle = math.fmod(dt*.3+angle, 360)
-  renderedText.modelTransform = logoTranslationMatrix*MatrixRotZ(angle)
-  tasklet.window.context:swapBuffers()
-end
-
 function update(tasklet)
-  local currentTime = lost.platform.currentTimeMilliSeconds()
-  local dt = currentTime - lastUpdate 
-
   if not exportScreens then
-    doUpdate(tasklet, dt)
+    animator:update(tasklet, rootEntity)
   else
-    local fps = 24
-    local totalTime = 10000 -- msec
-    local stepDelta = 1000 / fps
-    local numFrames = totalTime / stepDelta
-    local screenShotPath="/Users/tony/screenshots/"
-    local filename = nil
-    for i=1,numFrames do
-      doUpdate(tasklet, stepDelta)
-      filename = screenShotPath..i..".tga" 
-      log.debug(filename)
-      tasklet.window.context:writeScreenshot(filename, false)
-    end
+    animator:dump(tasklet, rootEntity, 24, 10*1000, "/Users/tony/screens/")  
     running = false
   end
-  
-  lastUpdate = currentTime
-  
   return running
 end
 
