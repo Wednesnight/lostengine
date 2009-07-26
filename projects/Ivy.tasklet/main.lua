5edfef498d45f551c6904544a3fa61a98559317a
lobotony <lobotony666@yahoo.de>
2009-07-26 14:45:37 +0000

    added missing cam, added custom loader for absolute load, added loader code for images

--- projects/Ivy.tasklet/main.lua
+++ projects/Ivy.tasklet/main.lua
@@ -1,7 +1,17 @@
 require("lost.declarative.Context")
 
+local MatrixTranslation = lost.math.MatrixTranslation
+local Vec3 = lost.math.Vec3
+local Rect = lost.math.Rect
+
 hasWindow = true
 windowParams = lost.application.WindowParams("Ivy", lost.math.Rect(200,200,640,480))
+
+rootNode = nil
+currentPicNode = nil -- receives a quad that draws the dropped bitmap at 0,0
+-- create a custom loader that takes absolute filepaths 
+bitmapLoader = lost.resource.Loader.create()
+bitmapLoader:addRepository(lost.resource.FilesystemRepository.create("/"))
 
 function startup(tasklet)
   dcl = lost.declarative.Context(tasklet.loader)
@@ -11,8 +21,14 @@ function startup(tasklet)
     name = "rootNode",
     dcl.rg:ClearColor{color=lost.common.Color(1,1,1,1)},
     dcl.rg:Clear{mask=gl.GL_COLOR_BUFFER_BIT+gl.GL_DEPTH_BUFFER_BIT},
+    dcl.rg:Camera2D
+    {
+      name = "2D Cam",
+      viewport = Rect(0,0,windowParams.rect.width,windowParams.rect.height)
+    },
     dcl.rg:DepthTest{false}
   }
+  tasklet.waitForEvents = true
   return true
 end
 
@@ -30,4 +46,10 @@ function dropHandler(event)
   local dropEvent = lost.application.DropEvent.cast(event)
   local path = dropEvent.filename
   log.debug("dropped file: '"..path.."'")
+  local data = bitmapLoader:load(path)
+  log.debug("loaded data of size: "..data.size)
+  rootNode:remove(currentPicNode)
+  local meshQuad = lost.mesh.Quad2D.create(data, true)
+  currentPicNode = lost.rg.Draw.create(meshQuad)
+  rootNode:add(currentPicNode)
 end
