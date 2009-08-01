7e5aa341eac4b58eb5d5a189260e78cc0c8c70e8
lobotony <lobotony666@yahoo.de>
2009-08-01 13:45:59 +0000

    starting rewrite

--- projects/lostengine/lost/lua/lost/declarative/Context.lua
+++ projects/lostengine/lost/lua/lost/declarative/Context.lua
@@ -7,22 +7,20 @@ require("lost.declarative.Font")
 require("lost.declarative.Mesh")
 require("lost.declarative.Gl")
 require("lost.declarative.Resource")
-require("lost.declarative.Guiro2")
+require("lost.declarative.Guiro")
 
 lost.common.Class "lost.declarative.Context"
 {
-  log.debug("--- table shit")
 }
 
 function Context:create(loader)
-  log.debug("--- create shit")
   self.loader = loader
   self.rg = lost.declarative.Rg(self.loader)
   self.font = lost.declarative.Font(self.loader)
   self.mesh = lost.declarative.Mesh(self.loader)
   self.gl = lost.declarative.Gl(self.loader)
   self.resource = lost.declarative.Resource(self.loader)
-  self.guiro2 = lost.declarative.Guiro2(self.loader)
+  self.guiro = lost.declarative.Guiro(self.loader)
 end
 
 
