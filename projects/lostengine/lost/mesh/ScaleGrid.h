ede3460905d36ce729c89c9206ab541a211baa48
lobotony <lobotony666@yahoo.de>
2009-08-26 20:49:24 +0000

    fixed ScaleGrid, added testcode to MeshTest

--- projects/lostengine/lost/mesh/ScaleGrid.h
+++ projects/lostengine/lost/mesh/ScaleGrid.h
@@ -61,53 +61,55 @@ struct ScaleGrid : public MESHTYPE
     // 
     // 0,3,12,15 are the corners of the provided rect
     // all others are derived from the l/r/t/b measurement that were provided.
-    
-    float dx = tex->dataMaxX() - right;
-    float dy = tex->dataMaxY() - top;
-    
+        
     // first, we calculate the vertex coordinates relative to 0,0 since these are the pixel coordinates 
     // we need to calculate the texel coordinates from the texture.
     math::Rect zr(0,0,rect.width, rect.height); // zero based rect for the first pass of the calculations
-    this->vertexData[0] = VertexType(zr.topLeft());
-    this->vertexData[1] = VertexType(left, zr.top());
-    this->vertexData[2] = VertexType(zr.right()-dx, zr.top());
-    this->vertexData[3] = VertexType(zr.topRight());
-    this->vertexData[4] = VertexType(zr.left(), zr.top()-dy);
-    this->vertexData[5] = VertexType(left, zr.top()-dy);
-    this->vertexData[6] = VertexType(zr.right()-dx, zr.top()-dy);
-    this->vertexData[7] = VertexType(zr.right(), zr.top()-dy);
-    this->vertexData[8] = VertexType(zr.left(), bottom);
-    this->vertexData[9] = VertexType(left, bottom);
-    this->vertexData[10] = VertexType(zr.right()-dx, bottom);
-    this->vertexData[11] = VertexType(zr.right(), bottom);
-    this->vertexData[12] = VertexType(zr.bottomLeft());
-    this->vertexData[13] = VertexType(left, zr.bottom());
-    this->vertexData[14] = VertexType(zr.right()-dx, zr.bottom());
-    this->vertexData[15] = VertexType(zr.bottomRight());
-
-
-    float mx = tex->dataMaxX();
-    float my = tex->dataMaxY();
-
-    this->texCoordData[0] = tex->normalisedCoord(VertexType(0,my));
-    this->texCoordData[1] = tex->normalisedCoord(VertexType(left, my));
-    this->texCoordData[2] = tex->normalisedCoord(VertexType(right, my));
-    this->texCoordData[3] = tex->normalisedCoord(VertexType(mx, my));
+    float h = rect.height;
+    float w = rect.width;
+    this->vertexData[0] = VertexType(0,h);
+    this->vertexData[1] = VertexType(left, h);
+    this->vertexData[2] = VertexType(w-right, h);
+    this->vertexData[3] = VertexType(w,h);
     
-    this->texCoordData[4] = tex->normalisedCoord(VertexType(0,top));
-    this->texCoordData[5] = tex->normalisedCoord(VertexType(left,top));
-    this->texCoordData[6] = tex->normalisedCoord(VertexType(right, top));
-    this->texCoordData[7] = tex->normalisedCoord(VertexType(mx, top));
+    this->vertexData[4] = VertexType(0, h-top);
+    this->vertexData[5] = VertexType(left, h-top);
+    this->vertexData[6] = VertexType(w-right, h-top);
+    this->vertexData[7] = VertexType(w, h-top);
+
+    this->vertexData[8] = VertexType(0, bottom);
+    this->vertexData[9] = VertexType(left, bottom);
+    this->vertexData[10] = VertexType(w-right, bottom);
+    this->vertexData[11] = VertexType(w, bottom);
+
+    this->vertexData[12] = VertexType(0,0);
+    this->vertexData[13] = VertexType(left, 0);
+    this->vertexData[14] = VertexType(w-right, 0);
+    this->vertexData[15] = VertexType(w,0);
+
+
+    float tw = std::min(tex->dataWidth, tex->width);
+    float th = std::min(tex->dataHeight, tex->height);
+
+    this->texCoordData[0] = tex->normalisedCoord(VertexType(0,th));
+    this->texCoordData[1] = tex->normalisedCoord(VertexType(left, th));
+    this->texCoordData[2] = tex->normalisedCoord(VertexType(tw-right, th));
+    this->texCoordData[3] = tex->normalisedCoord(VertexType(tw, th));
+    
+    this->texCoordData[4] = tex->normalisedCoord(VertexType(0,th-top));
+    this->texCoordData[5] = tex->normalisedCoord(VertexType(left,th-top));
+    this->texCoordData[6] = tex->normalisedCoord(VertexType(tw-right, th-top));
+    this->texCoordData[7] = tex->normalisedCoord(VertexType(tw, th-top));
     
     this->texCoordData[8] = tex->normalisedCoord(VertexType(0,bottom));
     this->texCoordData[9] = tex->normalisedCoord(VertexType(left,bottom));
-    this->texCoordData[10] = tex->normalisedCoord(VertexType(right, bottom));
-    this->texCoordData[11] = tex->normalisedCoord(VertexType(mx, bottom));
+    this->texCoordData[10] = tex->normalisedCoord(VertexType(tw-right, bottom));
+    this->texCoordData[11] = tex->normalisedCoord(VertexType(tw, bottom));
     
     this->texCoordData[12] = tex->normalisedCoord(VertexType(0,0));
     this->texCoordData[13] = tex->normalisedCoord(VertexType(left,0));
-    this->texCoordData[14] = tex->normalisedCoord(VertexType(right,0));
-    this->texCoordData[15] = tex->normalisedCoord(VertexType(mx, 0));
+    this->texCoordData[14] = tex->normalisedCoord(VertexType(tw-right,0));
+    this->texCoordData[15] = tex->normalisedCoord(VertexType(tw, 0));
     
     // and since the input rect defines the geometry and we built it zero based up to here,
     // we need to add the origin of the input rect to all previously calculated vertices
