03182ba4ff922d542a947bf94c229e027635866a
jomanto <jomanto@gmx.net>
2009-08-28 23:52:47 +0000

    obsolete

--- sandbox/LuaAppTest/KeyEvent.cpp
+++
@@ -1,4 +0,0 @@
-#include "KeyEvent.h"
-
-const std::string KeyEvent::KEY_DOWN = "keyDown";
-const std::string KeyEvent::KEY_UP = "keyUp";
--- sandbox/LuaAppTest/MouseEvent.cpp
+++
@@ -1,5 +0,0 @@
-#include "MouseEvent.h"
-
-const std::string MouseEvent::MOUSE_DOWN = "mouseDown";
-const std::string MouseEvent::MOUSE_UP = "mouseUp";
-const std::string MouseEvent::MOUSE_MOVE = "mouseMove";
