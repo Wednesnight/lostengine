18ad8262988f0f27a5158eda928bc0d5eadb68bd
lobotony <lobotony666@yahoo.de>
2009-09-02 19:18:49 +0000

    started of work on Button, added flip flag to ScaleGRid and declarative context

--- projects/lostengine/lost/lua/lost/guiro/Button.lua
+++ projects/lostengine/lost/lua/lost/guiro/Button.lua
@@ -5,9 +5,14 @@ require("lost.guiro.Image")
 
 lost.common.Class "lost.guiro.Button" "lost.guiro.View" {}
 
+Button.STATE_NORMAL = "normal"
+Button.STATE_HOVER = "hover"
+Button.STATE_PUSHED = "pushed"
+Button.STATE_DISABLED = "disabled"
+
 function Button:constructor()
 	lost.guiro.View.constructor(self)
-	
+	self._state = Button.STATE_NORMAL
 end
 
 
