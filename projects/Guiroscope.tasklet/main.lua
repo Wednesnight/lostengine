ff70411dbebd1a14f7c350909fdc13056312b992
jomanto <jomanto@gmx.net>
2009-09-07 09:08:19 +0000

    fixed Screen:updateLayout() and HBox updates

--- projects/Guiroscope.tasklet/main.lua
+++ projects/Guiroscope.tasklet/main.lua
@@ -1,11 +1,17 @@
 -- Guiroscope main.lua
+
+require("lost.declarative.Context")
+
 local WindowParams = lost.application.WindowParams
 local Rect = lost.math.Rect
 local Vec2 = lost.math.Vec2
 local Vec3 = lost.math.Vec3
 local Color = lost.common.Color
-
-require("lost.declarative.Context")
+local Bounds = lost.guiro.Bounds
+local xabs = lost.guiro.xabs
+local yabs = lost.guiro.yabs
+local wabs = lost.guiro.wabs
+local habs = lost.guiro.habs
 
 windowParams = WindowParams("Guiroscope", Rect(300,300,800,600))
 
@@ -23,14 +29,13 @@ function startup(tasklet)
   dcl = lost.declarative.Context(tasklet.loader)
 
   screen = require("ui")
-
   -- FIXME: the following few lines could be put into a helpfer function in Screen
   -- that associates a Screen instance with a tasklet and performs some final updates and an initial redraw
   -- then again a redraw might not be what you want in all use cases
   screen:listenTo(tasklet.eventDispatcher)
-  screen.currentGlobalRect = lost.math.Rect(0,0,windowParams.rect.width, windowParams.rect.height)
+  screen.bounds = Bounds(xabs(0), yabs(0), wabs(windowParams.rect.width), habs(windowParams.rect.height))
   -- update needs to be called later since the ui loader code triggers quite a few deferred updates
-  callLater(function() screen:updateLayout() end) 
+  callLater(function() screen:updateLayout(true) end) 
 
   -- running state
   running = true
