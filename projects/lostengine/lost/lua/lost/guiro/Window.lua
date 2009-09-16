88545f1dd20c623adab1ddbcbd38887ac73f44d1
jomanto <jomanto@gmx.net>
2009-09-16 21:50:03 +0000

    removed fixed ContentView size from ScrollView, added setter/getter for ContentView size, fixed Scrollbar

--- projects/lostengine/lost/lua/lost/guiro/Window.lua
+++ projects/lostengine/lost/lua/lost/guiro/Window.lua
@@ -8,16 +8,3 @@ lost.common.Class "lost.guiro.Window" "lost.guiro.View" {}
 function Window:constructor()
   lost.guiro.View.constructor(self)
 end
-
-function Window:contentView(view)
-  if view ~= nil then
-    self._contentView = view
-    lost.guiro.View.addSubview(self, view)
-  else
-    return self._contentView
-  end
-end
-
-function Window:addSubview(newview, pos)
-  self._contentView:addSubview(newview, pos)
-end
