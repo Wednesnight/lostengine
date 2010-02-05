9d5aef1e142e3662594e0a438d5fc57d84fe9e29
lobotony <lobotony666@yahoo.de>
2010-02-05 08:55:23 +0000

    refactored Quad

--- projects/lostengine/lost/mesh/Quad.h
+++ projects/lostengine/lost/mesh/Quad.h
@@ -21,101 +21,19 @@ typedef lost::shared_ptr<Quad> QuadPtr;
 struct Quad : public Mesh
 {
   // doesn't create any data or buffers, only a quad with a default drawMode and Material
-  Quad()
-  {
-    this->drawMode = GL_TRIANGLES;
-  }
-  
+  Quad();  
   // creates a white rectangle with the given size and position.
-  Quad(const math::Rect& inRect)
-  {
-    gl::BufferLayout layout;
-    layout.add(gl::ET_vec2_f32, gl::UT_vertex, 0);
-    this->resetBuffers(layout, gl::ET_u32);
-    
-    this->drawMode = GL_TRIANGLES;
-    boost::uint32_t numQuads = 1;
-    boost::uint32_t numVertices = numQuads*4;
-    boost::uint32_t numIndices = numQuads*6;
-    
-    this->_vertexBuffer->reset(numVertices);
-    this->_indexBuffer->reset(numIndices);
-    
-    createIndices(0);
-    createVertices(0,inRect);
-  }
+  Quad(const math::Rect& inRect);
   
   // tries to build a texture from the provided file data and 
   // builds a quad with the size of the data the texture was created with,
   // flipping the texture coords vertically if required.
   // This is essentially a shortcut for creating quads with the same extents as 
   // a loaded bitmap. The default Material has a white draw color.
-  Quad(common::DataPtr data, bool flip=true)
-  {
-    gl::BufferLayout layout;
-    layout.add(gl::ET_vec2_f32, gl::UT_vertex, 0);
-    layout.add(gl::ET_vec2_f32, gl::UT_texcoord0, 0);
-    this->resetBuffers(layout, gl::ET_u32);
-
-    this->drawMode = GL_TRIANGLES;
-    gl::TexturePtr tex(new gl::Texture(data));
-    math::Rect rect(0,0,tex->dataWidth, tex->dataHeight);
-    this->material->textures.push_back(tex);
-    boost::uint32_t numQuads = 1;
-    boost::uint32_t numVertices = numQuads*4;
-    boost::uint32_t numIndices = numQuads*6;
-    
-    this->_vertexBuffer->reset(numVertices);
-    this->_indexBuffer->reset(numIndices);
-    
-    createIndices(0);
-    createVertices(0, rect);
-    createTexCoords(0,flip);
-  }
-  
-  Quad(gl::TexturePtr tex, bool flip=true)
-  {
-    gl::BufferLayout layout;
-    layout.add(gl::ET_vec2_f32, gl::UT_vertex, 0);
-    layout.add(gl::ET_vec2_f32, gl::UT_texcoord0, 0);
-    this->resetBuffers(layout, gl::ET_u32);
-
-    this->drawMode = GL_TRIANGLES;
-    math::Rect rect(0,0,tex->dataWidth, tex->dataHeight);
-    this->material->textures.push_back(tex);
-    boost::uint32_t numQuads = 1;
-    boost::uint32_t numVertices = numQuads*4;
-    boost::uint32_t numIndices = numQuads*6;
-
-    this->_vertexBuffer->reset(numVertices);
-    this->_indexBuffer->reset(numIndices);
-
-    createIndices(0);
-    createVertices(0, rect);
-    createTexCoords(0,flip);    
-  }
-  
+  Quad(common::DataPtr data, bool flip=true);  
+  Quad(gl::TexturePtr tex, bool flip=true);  
   // creates a group of independent rects, all inside one mesh
-  Quad(const std::vector<math::Rect>& rects)
-  {
-    gl::BufferLayout layout;
-    layout.add(gl::ET_vec2_f32, gl::UT_vertex, 0);
-    this->resetBuffers(layout, gl::ET_u32);
-
-    this->drawMode = GL_TRIANGLES;
-    boost::uint32_t numQuads = rects.size();
-    boost::uint32_t numVertices = numQuads*4;
-    boost::uint32_t numIndices = numQuads*6;
-
-    this->_vertexBuffer->reset(numVertices);
-    this->_indexBuffer->reset(numIndices);
-
-    for(boost::uint32_t i=0; i<numQuads; ++i)
-    {
-      createIndices(i);
-      createVertices(i,rects[i]);
-    }
-  }
+  Quad(const std::vector<math::Rect>& rects);
   
   // tries to create rects.size() number of quads, texturing them with tex, using the provided 
   // pixelCoords to calculate texture cooridnates.
@@ -123,43 +41,12 @@ struct Quad : public Mesh
   Quad(const std::vector<math::Rect>& rects,
        gl::TexturePtr tex,
        const std::vector<math::Rect>& pixelCoords,
-       bool flip)
-  {
-    this->init(rects, tex, pixelCoords, flip);
-  }
+       bool flip);
   
   void init(const std::vector<math::Rect>& rects,
             gl::TexturePtr tex,
             const std::vector<math::Rect>& pixelCoords,
-            bool flip)
-  {
-    if(rects.size() != pixelCoords.size())
-    {
-      throw std::runtime_error("number of rects and pixelCoords must match");
-    }
-    
-    gl::BufferLayout layout;
-    layout.add(gl::ET_vec2_f32, gl::UT_vertex, 0);
-    layout.add(gl::ET_vec2_f32, gl::UT_texcoord0, 0);
-    this->resetBuffers(layout, gl::ET_u32);
-    
-    this->drawMode = GL_TRIANGLES;
-    this->material->textures.clear();
-    this->material->textures.push_back(tex);
-    boost::uint32_t numQuads = rects.size();
-    boost::uint32_t numVertices = numQuads*4;
-    boost::uint32_t numIndices = numQuads*6;
-    
-    this->_vertexBuffer->reset(numVertices);
-    this->_indexBuffer->reset(numIndices);
-    
-    for(boost::uint32_t i=0; i<numQuads; ++i)
-    {
-      createIndices(i);
-      createVertices(i, rects[i]);
-      createTexCoords(i, 0, pixelCoords[i], flip);
-    }
-  }
+            bool flip);
   
   static QuadPtr create() { return QuadPtr(new Quad()); }
   static QuadPtr create(const math::Rect& inRect) { return QuadPtr(new Quad(inRect)); }
@@ -167,92 +54,30 @@ struct Quad : public Mesh
   static QuadPtr create(common::DataPtr data, bool flip=true) { return QuadPtr(new Quad(data, flip)); }
   static QuadPtr create(gl::TexturePtr tex, bool flip=true) { return QuadPtr(new Quad(tex, flip)); }
   
-  virtual ~Quad() {}
+  virtual ~Quad();
   
   // writes the indices for a given quad into the provided buffer
   // two triangles, counterclockwise
-  void createIndices(boost::uint32_t quadNum)
-  { 
-    const boost::uint32_t numIndicesPerQuad = 6;
-    boost::uint32_t indexOffset = quadNum*numIndicesPerQuad;
-    boost::uint32_t vertexOffset = quadNum*4;
-    this->setIndex(indexOffset+0, vertexOffset + 0);
-    this->setIndex(indexOffset+1, vertexOffset + 1);
-    this->setIndex(indexOffset+2, vertexOffset + 2);
-    this->setIndex(indexOffset+3, vertexOffset + 2);
-    this->setIndex(indexOffset+4, vertexOffset + 3);
-    this->setIndex(indexOffset+5, vertexOffset + 0);    
-  }
-  
-  void createVertices(boost::uint32_t quadNum, const math::Rect& inRect)
-  {
-    boost::uint32_t verticesPerQuad = 4;
-    boost::uint32_t offset = quadNum*verticesPerQuad;
-      
-    this->setVertex(offset+0,math::Vec2(inRect.x,inRect.y));
-    this->setVertex(offset+1,math::Vec2(inRect.x+inRect.width,inRect.y));
-    this->setVertex(offset+2,math::Vec2(inRect.x+inRect.width,inRect.y+inRect.height));
-    this->setVertex(offset+3,math::Vec2(inRect.x,inRect.y+inRect.height));
-  }
-              
-  void updateSize(const math::Vec2& size)
-  {
-    math::Rect rect(0, 0, size.width, size.height);
-    createVertices(0, rect);
-  }
+  void createIndices(boost::uint32_t quadNum);  
+  void createVertices(boost::uint32_t quadNum, const math::Rect& inRect);
+  void updateSize(const math::Vec2& size);
 
   // recalculates the texture coordinates for a given quad and texture 0 so the textures data stretechs over the
   // whole quad.
   void createTexCoords(boost::uint32_t quadNum,
-                       bool flip=true)
-  {
-    createTexCoords(quadNum, 0, flip);
-  }
+                       bool flip=true);
 
   // recalculates the texture coordinates for a given quad and texture so the textures data stretechs over the
   // whole quad.
   void createTexCoords(boost::uint32_t quadNum,       // the index of the quad whose texture coordinates should be updated
                        boost::uint32_t texNum,        // the texture to use fo rthe texcoord generation
-                       bool flip=true)                // if flip 0 true, texture coordinates will be flipped vertically
-  {
-    gl::TexturePtr tex = this->material->textures[texNum];
-    math::Rect pixelCoords(0,0,tex->dataWidth, tex->dataHeight);
-    createTexCoords(quadNum, texNum, pixelCoords, flip);
-  }
+                       bool flip=true);               // if flip 0 true, texture coordinates will be flipped vertically
   
   // recalculates the texture coordinates for a given quad
   void createTexCoords(boost::uint32_t quadNum,       // the index of the quad whose texture coordinates should be updated
                        boost::uint32_t texNum,        // the texture to use fo rthe texcoord generation
                        const math::Rect& pixelRect,   // a rect describing a section of the texture in pixel coordinates
-                       bool flip=true)                // if flip 0 true, texture coordinates will be flipped vertically
-  {
-    // FIXME: since texcoords can have any type, we'd probably need something like 
-    // TexCoordType::maxX instead of 1 etc., but it'll work for now
-    // i.e. the values might have to be converted from normalised float texture coordinates 
-    // to something else like uint8_t etc.
-    gl::TexturePtr tex = this->material->textures[texNum];
-
-    math::Vec2 bl = tex->normalisedCoord(pixelRect.bottomLeft());
-    math::Vec2 tr = tex->normalisedCoord(math::Vec2(pixelRect.x+pixelRect.width, pixelRect.y+pixelRect.height)/*pixelRect.topRight()*/);
-    
-    const boost::uint32_t texCoordsPerQuad = 4; 
-    boost::uint32_t offset = texCoordsPerQuad*quadNum;
-    
-    if(flip)
-    {
-      this->setTexCoord(offset+0, math::Vec2(bl.x,tr.y));
-      this->setTexCoord(offset+1, math::Vec2(tr.x,tr.y));
-      this->setTexCoord(offset+2, math::Vec2(tr.x,bl.y));
-      this->setTexCoord(offset+3, math::Vec2(bl.x,bl.y));
-    }
-    else
-    {
-      this->setTexCoord(offset+0, math::Vec2(bl.x,bl.y));
-      this->setTexCoord(offset+1, math::Vec2(tr.x,bl.y));
-      this->setTexCoord(offset+2, math::Vec2(tr.x,tr.y));
-      this->setTexCoord(offset+3, math::Vec2(bl.x,tr.y));
-    }
-  }
+                       bool flip=true);               // if flip 0 true, texture coordinates will be flipped vertically
 };
 }
 }
