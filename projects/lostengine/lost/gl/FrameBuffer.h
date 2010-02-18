b986d1c3af25c4112027b6719be8fd9c07d238c3
jomanto <jomanto@gmx.net>
2010-02-18 20:23:21 +0000

    cleanup, bugfixing and rewrote parts of FrameBuffer

--- projects/lostengine/lost/gl/FrameBuffer.h
+++ projects/lostengine/lost/gl/FrameBuffer.h
@@ -1,13 +1,15 @@
 #ifndef LOST_GL_FRAMEBUFFER_H
 #define LOST_GL_FRAMEBUFFER_H
 
-#include "lost/gl/gltypes.h"
-#include "lost/gl/RenderBuffer.h"
-#include "lost/gl/Texture.h"
+#include <boost/cstdint.hpp>
+#include <vector>
 #include <map>
+#include "lost/gl/Texture.h"
+#include "lost/gl/RenderBuffer.h"
 
 namespace lost
 {
+  namespace math { struct Vec2; }
   namespace gl
   {
     struct Context;
@@ -18,78 +20,84 @@ namespace lost
 
     struct FrameBuffer
     {
+
+      struct Attachment;
+      typedef lost::shared_ptr<Attachment> AttachmentPtr;
+
+      struct Attachment
+      {
+
+        enum UsageType
+        {
+          UT_texture      = 0,
+          UT_renderBuffer = 1
+        };
+
+      private:
+        void createTexture();
+        void updateTexture();
+        void createRenderBuffer();
+        void updateRenderBuffer();
+
+      public:
+        math::Vec2 size;
+        GLenum     bitFormat;
+        UsageType  usageType;
+
+        TexturePtr      texture;      // for UsageType UT_texture
+        RenderBufferPtr renderBuffer; // for UsageType UT_renderBuffer
+
+        static AttachmentPtr create(const math::Vec2& size, GLenum bitFormat, UsageType usageType = UT_texture);
+        static AttachmentPtr create(const TexturePtr& texture);            // for UsageType UT_texture
+        static AttachmentPtr create(const RenderBufferPtr& renderBuffer);  // for UsageType UT_renderBuffer
+
+        Attachment(const math::Vec2& size, GLenum bitFormat, UsageType usageType = UT_texture);
+        Attachment(const TexturePtr& texture);            // for UsageType UT_texture
+        Attachment(const RenderBufferPtr& renderBuffer);  // for UsageType UT_renderBuffer
+
+        ~Attachment();
+
+        void attach(GLenum target);
+
+        void resize(const math::Vec2& size);
+
+        void bind();
+        
+      };
+      
     private:
-      // attaches the renderbuffer to the currently bound framebuffer at the specified attachment target
-      void attach(GLenum target, RenderBufferPtr inRenderBuffer);
+      GLuint buffer;
 
-      void attach(GLenum target, TexturePtr inTexture);
-
+      void bindFramebuffer();
+      void unbindFramebuffer();
 
     public:
+      math::Vec2 size;
 
-      // helper functions for easier creation of framebuffers, help catch common pitfalls
-      // color attachment creators, either texture or renderbuffer
-      // textures might have to obey certain hardware/driver limitations that renderbuffers
-      // don't necessarily have to follow.
-      static TexturePtr createColorTexture(const math::Vec2& size, // size in pixels, result might vary
-                                           GLenum internalFormat); // format of data as stored on gfx card
-      static TexturePtr createDepthTexture(const math::Vec2& size, // size in pixels, result might vary
-                                           uint32_t bitDepth);      // desired bit depth, throws if bitdepth can'T be mapped to enum
+      std::map<boost::uint8_t, AttachmentPtr> colorBuffers;
+      AttachmentPtr                           depthBuffer;
+      AttachmentPtr                           stencilBuffer;
 
-      /** creates a FBO with texture attachments for color 0 and depth.
-       *
-       * @param size pixel width and height of FBO. Since the attachments are textures, the resulting
-       *             size might vary.
-       * @param colorFormat the pixel format of the data stored on the gfx card, usually GL_RGB or GL_RGBA
-       * @param bitDepth the bit depth of the depth buffer. In order to ease construction of the FBO,
-       *                 lower bitDepths will be tried if the initial bitDepth results in a !isComplete status.
-       *
-       * @return complete FBO    
-       * @throws runtime_error if the FBO couldn't be constructed.
-       */
-      static FrameBufferPtr createFrameBuffer(ContextPtr& ctx,
-                                              const math::Vec2& size,
-                                              GLenum colorFormat, // internal format of color attachment
-                                              uint32_t bitDepth); // desired bitdepth of depth attachment
-                                                                 // lower resolutions will be tried if 
+      static FrameBufferPtr create(const math::Vec2& size, GLenum colorBits = -1, GLenum depthBits = -1, GLenum stencilBits = -1);
 
-      FrameBuffer();
-      virtual ~FrameBuffer();
+      FrameBuffer(const math::Vec2& size, GLenum colorBits = -1, GLenum depthBits = -1, GLenum stencilBits = -1);
+      ~FrameBuffer();
 
-      void enable();
-      void disable();
-
-      // attach a color buffer to one of 16 slots, zero indexed
-      // no need to proivde GLenum ,just index them
-      void attachColor(int index, RenderBufferPtr inRenderBuffer);
-      void attachStencil(RenderBufferPtr inRenderBuffer);
-      void attachDepth(RenderBufferPtr inRenderBuffer);
-
-      // attach a color buffer to one of 16 slots, zero indexed
-      // no need to proivde GLenum ,just index them
-      void attachColor(int index, TexturePtr inTexture);
-      void attachStencil(TexturePtr inTexture);
-      void attachDepth(TexturePtr inTexture);
+      void attachColorBuffer(boost::uint8_t index, const TexturePtr& buffer);
+      void attachColorBuffer(boost::uint8_t index, const AttachmentPtr& buffer);
+      void attachDepthBuffer(const TexturePtr& buffer);
+      void attachDepthBuffer(const AttachmentPtr& buffer);
+      void attachStencilBuffer(const TexturePtr& buffer);
+      void attachStencilBuffer(const AttachmentPtr& buffer);
+      
       GLenum status();
       bool isComplete();
+      void check();
+      
+      void resize(const math::Vec2& size);
+      
+      void bind();
 
-      GLuint buffer;
-      GLint  previousFramebuffer;
-      
-      // these variables hold the actual attachments so exterior code 
-      // doesn't have to, and the FBO can be passed around as a single,
-      // self-contained entity.
-      // due to the nature an dflexibility of the whole thing, the names
-      // are a bit redundant.
-      // not all of these must be set. The code that actually uses the FBO 
-      // should know what to expect in which slot.
-      std::map<uint8_t, TexturePtr> colorTextures; // index them with the integer, NOT the GLenum
-      TexturePtr depthTexture;
-      TexturePtr stencilTexture;
-      
-      std::map<uint8_t, RenderBufferPtr> colorBuffers; // index them with the integer, NOT the GLenum
-      RenderBufferPtr depthBuffer;
-      RenderBufferPtr stencilBuffer;
     };
 
   }
