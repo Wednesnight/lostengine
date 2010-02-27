1dd23426f7879898f128a5966d86511b82a775a5
lobotony <lobotony666@yahoo.de>
2010-02-27 23:40:43 +0000

    managed to build a completely filled pow2 disc texture

--- sandbox/rrtest.tasklet/main.lua
+++ sandbox/rrtest.tasklet/main.lua
@@ -6,22 +6,34 @@ using "lost.application.WindowParams"
 using "lost.common.Color"
 using "lost.bitmap.Bitmap"
 
-screensize = Vec2(640,480)
+screensize = Vec2(512,512)
 windowParams = WindowParams("rrtest", Rect(50,50,screensize.x, screensize.y))
 
 running = true
 rootNode = nil
 dcl = nil
 
+-- creates a square texture with a white circle on transparent background
+-- n is log2(width)
+-- circle is centered and sized so it completely fills the bitmap used to create the texture
+function pow2discTexture(n)
+  local size = math.pow(2,n)
+  local bmp = Bitmap.create(size, size, lost.bitmap.COMPONENTS_RGBA)
+  local cx = size/2 - 0.5
+  local cy = cx
+  local cr = cy
+  bmp:disc(cx,cy,cr)
+  local defaultParams = lost.gl.Texture.Params()
+  local tex = lost.gl.Texture.create(bmp, defaultParams)  
+  return tex
+end
+
 function startup(tasklet)
   tasklet.name = "rrtest"
+  tasklet.waitForEvents = true
   tasklet.eventDispatcher:addEventListener(lost.application.KeyEvent.KEY_DOWN, keyHandler)  
   dcl = lost.declarative.Context(tasklet.loader)
 
-  local bmp = Bitmap.create(512, 512, lost.bitmap.COMPONENTS_RGBA)
-  bmp:disc(256,256,64)
-  local defaultParams = lost.gl.Texture.Params()
-  local tex = lost.gl.Texture.create(bmp, defaultParams)
 
   rootNode = dcl.rg:Node
   {
@@ -42,7 +54,7 @@ function startup(tasklet)
     {
       mesh = dcl.mesh:Quad
       {
-        texture = tex,
+        texture = pow2discTexture(9),
         material = 
         {
           blend = true
