db2cf0aa0fd7f8e4e903b5b1121eb31de8f0ee09
lobotony <lobotony666@yahoo.de>
2010-03-01 23:54:56 +0000

    more refactoring of mac stuff, added some simple iphone implementations and empty files

---
+++ projects/lostengine/lost/application/iphone/ApplicationTaskletSpawn.cpp
@@ -0,0 +1,16 @@
+#include "lost/application/Application.h"
+#include "lost/application/SingleThreadedTasklet.h"
+#include "lost/application/SpawnTaskletEvent.h"
+
+// needs to be in a separate file that's not .mm because objective-c++ can't handle the Tasklet/luabind code
+namespace lost
+{
+  namespace application
+  {
+    // is platform specific because we need to determine the default type for each platform which might eb compile time dependent
+    void Application::taskletSpawn(const SpawnTaskletEventPtr& event)
+    {
+      addTasklet(new SingleThreadedTasklet(event->loader));
+    }
+  }
+}
---
+++ projects/lostengine/lost/application/mac/ApplicationDelegate.mm
@@ -0,0 +1,64 @@
+#import "lost/application/mac/ApplicationDelegate.h"
+#include <Foundation/NSThread.h>
+#include "lost/common/Logger.h"
+#include "lost/application/ApplicationEvent.h"
+#include "lost/application/Application.h"
+#include "lost/event/EventDispatcher.h"
+
+@implementation ApplicationDelegate
+
+- (void) dummyThread:(id)arg;
+{
+}
+
+-(id)init
+{
+    if(self = [super init])
+    {
+      // make this application multithreaded by spawning a dummy thread that exits immediately
+      DOUT("isMultiThreaded: " << (bool)[NSThread isMultiThreaded]);
+      [NSThread detachNewThreadSelector:@selector(dummyThread:) toTarget:self withObject:nil];
+      DOUT("isMultiThreaded: " << (bool)[NSThread isMultiThreaded]);        
+    }
+    return self;
+}
+
+- (void)applicationDidFinishLaunching: (NSNotification *)notification
+{
+  DOUT("applicationDidFinishLaunching");
+  if (parent)
+  {
+    lost::shared_ptr<lost::application::ApplicationEvent> event(new lost::application::ApplicationEvent(lost::application::ApplicationEvent::RUN()));
+    parent->eventDispatcher->dispatchEvent(event);
+  }
+}
+
+- (void)applicationWillTerminate: (NSNotification *)notification
+{
+  DOUT("applicationWillTerminate");
+}
+
+- (void)setParent: (lost::application::Application*)newParent
+{
+  parent = newParent;
+}
+
+- (void)terminate
+{
+  [[NSApplication sharedApplication] terminate: nil];
+}
+
+- (void) quitAction: (id)sender
+{
+  if (parent)
+  {
+    parent->quit();
+  }
+}
+
+- (void)processApplicationEvents
+{
+  if (parent) parent->eventDispatcher->processEvents();
+}
+
+@end
