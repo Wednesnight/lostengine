37b55716ddfcd4f58dcaec241cd83590f74aa88d
jomanto <jomanto@gmx.net>
2010-03-09 16:40:02 +0000

    fixed windows Tasklet lifecycle (again)

--- projects/lostengine/lost/application/windows/Tasklet.cpp
+++ projects/lostengine/lost/application/windows/Tasklet.cpp
@@ -3,9 +3,13 @@
 #include "lost/application/TaskletEvent.h"
 #include "lost/application/Window.h"
 #include "lost/event/EventDispatcher.h"
+#include <sstream>
+#include <stdexcept>
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+
+using namespace std;
 
 namespace lost
 {
@@ -28,30 +32,49 @@ namespace lost
     {
       isAlive = true;
       init();
-      if (startup())
+      bool hasError = false;
+      try
       {
-        while (hiddenMembers->thread->get_state() == fhtagn::threads::tasklet::RUNNING && update())
+        // make sure that our GL context is the current context
+        if(window != NULL)
         {
-          render();
-
-          if(waitForEvents)
-          {
-            if (window) WaitMessage();
-              else eventDispatcher->waitForEvents();
-          }
-          MSG msg;
-          while (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
-          {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-          }
-          processEvents();
+          window->context->makeCurrent();
         }
-        shutdown();
+        if (startup())
+        {
+          while (hiddenMembers->thread->get_state() == fhtagn::threads::tasklet::RUNNING && update())
+          {
+            render();
+
+            if(waitForEvents)
+            {
+              if (window) WaitMessage();
+                else eventDispatcher->waitForEvents();
+            }
+            MSG msg;
+            while (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
+            {
+              TranslateMessage(&msg);
+              DispatchMessage(&msg);
+            }
+            processEvents();
+          }
+          shutdown();
+        }
       }
+      catch (...)
+      {
+        hasError = true;
+      }
+      isAlive = false;
       dispatchApplicationEvent(TaskletEventPtr(new TaskletEvent(TaskletEvent::DONE(), this)));
       cleanup();
-      isAlive = false;
+      if (hasError)
+      {
+        ostringstream os;
+        os << "Tasklet terminated with error: " << name;
+        throw runtime_error(os.str());
+      }
     }
 
     void Tasklet::stop()
