2c548d6e2e2586bb611475147bcc21069cb4b3fa
lobotony <lobotony666@yahoo.de>
2010-03-12 01:29:28 +0000

    removed OES suffix from GL functions and constants because we're building against ES 2 now, removed or disabled some obsolete functions

--- projects/lostengine/lost/lgl/lgl_gles.h
+++ projects/lostengine/lost/lgl/lgl_gles.h
@@ -3,101 +3,86 @@
 
 #include "lost/gl/gl.h"
 
-#define LGL_RENDERBUFFER                 GL_RENDERBUFFER_OES
-#define LGL_FRAMEBUFFER                  GL_FRAMEBUFFER_OES
-#define LGL_RENDERBUFFER_WIDTH           GL_RENDERBUFFER_WIDTH_OES
-#define LGL_RENDERBUFFER_HEIGHT          GL_RENDERBUFFER_HEIGHT_OES
-#define LGL_RENDERBUFFER_INTERNAL_FORMAT GL_RENDERBUFFER_INTERNAL_FORMAT_OES
-#define LGL_RENDERBUFFER_RED_SIZE        GL_RENDERBUFFER_RED_SIZE_OES
-#define LGL_RENDERBUFFER_GREEN_SIZE      GL_RENDERBUFFER_GREEN_SIZE_OES
-#define LGL_RENDERBUFFER_BLUE_SIZE       GL_RENDERBUFFER_BLUE_SIZE_OES
-#define LGL_RENDERBUFFER_ALPHA_SIZE      GL_RENDERBUFFER_ALPHA_SIZE_OES
-#define LGL_RENDERBUFFER_DEPTH_SIZE      GL_RENDERBUFFER_DEPTH_SIZE_OES
-#define LGL_RENDERBUFFER_STENCIL_SIZE    GL_RENDERBUFFER_STENCIL_SIZE_OES
-#define LGL_COLOR_ATTACHMENT0            GL_COLOR_ATTACHMENT0_OES
-#define LGL_STENCIL_ATTACHMENT           GL_STENCIL_ATTACHMENT_OES
-#define LGL_DEPTH_ATTACHMENT             GL_DEPTH_ATTACHMENT_OES
-#define LGL_DEPTH_COMPONENT              GL_DEPTH_COMPONENT16_OES
-#define LGL_DEPTH_COMPONENT16            GL_DEPTH_COMPONENT16_OES
+#define LGL_RENDERBUFFER                 GL_RENDERBUFFER
+#define LGL_FRAMEBUFFER                  GL_FRAMEBUFFER
+#define LGL_RENDERBUFFER_WIDTH           GL_RENDERBUFFER_WIDTH
+#define LGL_RENDERBUFFER_HEIGHT          GL_RENDERBUFFER_HEIGHT
+#define LGL_RENDERBUFFER_INTERNAL_FORMAT GL_RENDERBUFFER_INTERNAL_FORMAT
+#define LGL_RENDERBUFFER_RED_SIZE        GL_RENDERBUFFER_RED_SIZE
+#define LGL_RENDERBUFFER_GREEN_SIZE      GL_RENDERBUFFER_GREEN_SIZE
+#define LGL_RENDERBUFFER_BLUE_SIZE       GL_RENDERBUFFER_BLUE_SIZE
+#define LGL_RENDERBUFFER_ALPHA_SIZE      GL_RENDERBUFFER_ALPHA_SIZE
+#define LGL_RENDERBUFFER_DEPTH_SIZE      GL_RENDERBUFFER_DEPTH_SIZE
+#define LGL_RENDERBUFFER_STENCIL_SIZE    GL_RENDERBUFFER_STENCIL_SIZE
+#define LGL_COLOR_ATTACHMENT0            GL_COLOR_ATTACHMENT0
+#define LGL_STENCIL_ATTACHMENT           GL_STENCIL_ATTACHMENT
+#define LGL_DEPTH_ATTACHMENT             GL_DEPTH_ATTACHMENT
+#define LGL_DEPTH_COMPONENT              GL_DEPTH_COMPONENT16
+#define LGL_DEPTH_COMPONENT16            GL_DEPTH_COMPONENT16
 #define LGL_DEPTH_COMPONENT24            GL_DEPTH_COMPONENT24_OES
-#define LGL_FRAMEBUFFER_COMPLETE         GL_FRAMEBUFFER_COMPLETE_OES
+#define LGL_FRAMEBUFFER_COMPLETE         GL_FRAMEBUFFER_COMPLETE
 
-#define LGL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT   GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_OES
-#define LGL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT   GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_OES
-#define LGL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS   GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_OES
-#define LGL_FRAMEBUFFER_INCOMPLETE_FORMATS   GL_FRAMEBUFFER_INCOMPLETE_FORMATS_OES
-#define LGL_FRAMEBUFFER_UNSUPPORTED   GL_FRAMEBUFFER_UNSUPPORTED_OES
+#define LGL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT   GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT
+#define LGL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT   GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT
+#define LGL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS   GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS
+#define LGL_FRAMEBUFFER_INCOMPLETE_FORMATS   GL_FRAMEBUFFER_INCOMPLETE_FORMATS
+#define LGL_FRAMEBUFFER_UNSUPPORTED   GL_FRAMEBUFFER_UNSUPPORTED
 
-#define LGL_FRAMEBUFFER_BINDING          GL_FRAMEBUFFER_BINDING_OES
-
-static inline void lglOrtho(GLfloat left,
-              GLfloat right,
-              GLfloat bottom,
-              GLfloat top,
-              GLfloat near,
-              GLfloat far)
-{
-  glOrthof(left,
-           right,
-           bottom,
-           top,
-           near,
-           far);
-}
+#define LGL_FRAMEBUFFER_BINDING          GL_FRAMEBUFFER_BINDING
 
 static inline void lglGenRenderbuffers(GLsizei n, GLuint* renderbuffers)
 {
-  glGenRenderbuffersOES(n, renderbuffers);
+  glGenRenderbuffers(n, renderbuffers);
 }
 
 static inline void lglBindRenderbuffer(GLenum target, GLuint renderbuffer)
 {
-  glBindRenderbufferOES(target, renderbuffer);
+  glBindRenderbuffer(target, renderbuffer);
 }
 
 static inline void lglBindFramebuffer(GLenum target, GLuint framebuffer)
 {
-  glBindFramebufferOES(target, framebuffer);
+  glBindFramebuffer(target, framebuffer);
 }
 
 static inline void lglRenderbufferStorage(GLenum target, GLenum internalformat, GLsizei width, GLsizei height)
 {
-  glRenderbufferStorageOES(target, internalformat, width, height);
+  glRenderbufferStorage(target, internalformat, width, height);
 }
 
 static inline void lglGetRenderbufferParameteriv(GLenum target, GLenum pname, GLint* params)
 {
-  glGetRenderbufferParameterivOES(target, pname, params);
+  glGetRenderbufferParameteriv(target, pname, params);
 }
 
 static inline void lglDeleteRenderbuffers(GLsizei n, const GLuint* renderbuffers)
 {
-  glDeleteRenderbuffersOES(n, renderbuffers);
+  glDeleteRenderbuffers(n, renderbuffers);
 }
 
 static inline void lglDeleteFramebuffers(GLsizei n, const GLuint* framebuffers)
 {
-  glDeleteFramebuffersOES(n, framebuffers);
+  glDeleteFramebuffers(n, framebuffers);
 }
 
 static inline void lglGenFramebuffers(GLsizei n, GLuint* framebuffers)
 {
-  glGenFramebuffersOES(n, framebuffers);
+  glGenFramebuffers(n, framebuffers);
 }
 
 static inline void lglFramebufferRenderbuffer(GLenum target, GLenum attachment, GLenum renderbuffertarget, GLuint renderbuffer)
 {
-  glFramebufferRenderbufferOES(target, attachment, renderbuffertarget, renderbuffer);
+  glFramebufferRenderbuffer(target, attachment, renderbuffertarget, renderbuffer);
 }
 
 static inline void lglFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level)
 {
-  glFramebufferTexture2DOES(target, attachment, textarget, texture, level);
+  glFramebufferTexture2D(target, attachment, textarget, texture, level);
 }
 
 static inline GLenum lglCheckFramebufferStatus(GLenum target)
 {
-  return glCheckFramebufferStatusOES(target);
+  return glCheckFramebufferStatus(target);
 }
 
 #endif
