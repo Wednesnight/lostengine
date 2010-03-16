0521da5a7a19be1a975b9de6290cf1e7b21cf543
lobotony <lobotony666@yahoo.de>
2010-03-16 12:24:22 +0000

    #174 loader helper now returns null instead of throwing an exception upon file not found

--- projects/lostengine/lost/resource/Helper.cpp
+++ projects/lostengine/lost/resource/Helper.cpp
@@ -1,6 +1,7 @@
 #include "lost/resource/Helper.h"
 #include <fstream>
 #include <stdexcept>
+#include "lost/common/Logger.h"
 
 namespace lost
 {
@@ -12,9 +13,20 @@ common::DataPtr loadFromAbsolutePath(const std::string& inPath)
   common::DataPtr result(new common::Data);
   
   std::ifstream ifs( inPath.c_str(), std::ios_base::in | std::ios_base::binary );
-  if(!ifs.good()) { throw std::runtime_error( "couldn't open file: '"+ inPath +"'" ); }
+  if(!ifs.good()) 
+  {
+    // common case: the file wasn't found in a particular repository and we just return NULL, no need to panic
+//    DOUT("couldn't open file: '"+ inPath +"'");
+    common::DataPtr null;
+    return null;
+  }
   ifs.seekg(0,std::ios_base::end);
-  if(ifs.fail()) throw std::runtime_error("failed");
+  if(ifs.fail())
+  {
+    EOUT("seek failed!");
+    common::DataPtr null;
+    return null;
+  }
   unsigned long filesize = ifs.tellg();
   ifs.seekg(std::ios_base::beg);
   result->size = filesize;
