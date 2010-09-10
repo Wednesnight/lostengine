f75b3a1ac65feb02d9169c6d2e2b3ddb116d7154
lobotony <lobotony666@yahoo.de>
2010-09-10 19:16:07 +0000

    firs import

---
+++ sandbox/Aqua/config.lua
@@ -0,0 +1,13 @@
+return {
+  tasklet = {
+    name = "Aqua",
+    waitForEvents = false
+  },
+  window = {
+    title = "Aqua",
+    x = 200,
+    y = 200,
+    width = 640,
+    height = 480
+  }
+}
