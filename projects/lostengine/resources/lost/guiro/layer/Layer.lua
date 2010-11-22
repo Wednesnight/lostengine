17087cebf780a572ebeeebf99510da816e11a048
lobotony <lobotony666@yahoo.de>
2010-11-22 19:24:24 +0000

    removed ordering code from UI, moved MeshFactory into lostengine, started work on Layers

--- projects/lostengine/resources/lost/guiro/layer/Layer.lua
+++ projects/lostengine/resources/lost/guiro/layer/Layer.lua
@@ -2,10 +2,21 @@ module("lost.guiro.layer", package.seeall)
 
 lost.common.Class "lost.guiro.layer.Layer" {}
 
-function Layer:constructor(textureManager)
-	self.rootNode = nil
+function Layer:constructor()
+	self.renderNode = lost.rg.Node.create()
+	self.superLayer = nil
+  self.subLayers = {}
+	self.z = 0
 end
 
+function Layer:addSublayer(layer)
+end
+
+function Layer:removeLayer(layer)
+end
+
+function Layer:removeFromSuperLayer()
+end
 
 
 
