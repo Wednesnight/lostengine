ad76f50c24c4f82ad560622874784259025163c3
lobotony <lobotony666@yahoo.de>
2010-11-29 13:52:11 +0000

    removed scissoring, relaxed type checks in addSubview

--- projects/lostengine/resources/lost/guiro/view/UserInterface.lua
+++ projects/lostengine/resources/lost/guiro/view/UserInterface.lua
@@ -2,22 +2,8 @@ module("lost.guiro.view", package.seeall)
 
 require("lost.guiro.view.View")
 
---[[
-     UserInterface class
-  ]]
 lost.common.Class "lost.guiro.view.UserInterface" "lost.guiro.view.View" {}
 
 function UserInterface:constructor(textureManager)
   lost.guiro.view.View.constructor(self, textureManager)
-end
-
---[[
-    only accepts lost.guiro.controls.Window
-  ]]
-function UserInterface:addSubview(subview)
-  if subview:isDerivedFrom("lost.guiro.view.Window") then
-    lost.guiro.view.View.addSubview(self, subview)
-  else
-    error("UserInterface:addSubview() can only add subviews of type Window : ".. subview:className(), 2)    
-  end
 end
