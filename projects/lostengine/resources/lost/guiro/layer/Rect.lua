88bcf4209f5ec745d36f732ca864c70305dedca6
lobotony <lobotony666@yahoo.de>
2010-12-01 23:35:45 +0000

    added rectframe shader and more params to rect so it can be configured to a frame with variable width

--- projects/lostengine/resources/lost/guiro/layer/Rect.lua
+++ projects/lostengine/resources/lost/guiro/layer/Rect.lua
@@ -5,12 +5,37 @@ require("lost.guiro.Quad")
 
 lost.common.Class "lost.guiro.layer.Rect" "lost.guiro.layer.Layer" {}
 
+local Vec2 = lost.math.Vec2
+
+-- valid params:
+-- * all layer params
+-- * color: default is (1,1,1,1)
+-- * filled: true/false, default is true. If false, a frame with the specified width will be drawn
+-- * width: used if rect is not filled. Default is 0 for not filled, 1 for filled.
 function Rect:constructor(args)
   lost.guiro.layer.Layer.constructor(self, args)
   local t = args or {}
   self.quad = lost.guiro.Quad{}
   self.mesh = self.quad.mesh
-  self.mesh.material.shader = ui.shaderFactory:color()
+  if t.filled ~= nil then
+    self.filled = t.filled
+  else
+    self.filled = true
+  end
+  if t.width ~= nil then
+    self.width = t.width
+  else
+    if self.filled then
+      self.width = 0
+    else
+      self.width = 1
+    end
+  end
+  if self.filled then
+    self.mesh.material.shader = ui.shaderFactory:color()
+  else
+    self.mesh.material.shader = ui.shaderFactory:rectFrame()    
+  end
   self.mesh.material.color = t.color or lost.common.Color(1,1,1)
   self.drawNode = lost.rg.Draw.create(self.mesh)
   self.layerNodes:add(self.drawNode)
@@ -20,4 +45,8 @@ end
 function Rect:updateLayout()
   lost.guiro.layer.Layer.updateLayout(self)
   self.quad:update(self.rect)
+  if not self.filled then
+    self.mesh.material.uniforms:setFloat("width", self.width)
+    self.mesh.material.uniforms:set("size", Vec2(self.rect.width, self.rect.height))
+  end
 end
