ffc8fdd8b5fec877c96271abb8e6c75157880de2
lobotony <lobotony666@yahoo.de>
2010-12-02 16:27:13 +0000

    finalised rect so it supports gradients in all variations, added additional shaders, cleaned up shaders so more parts can be reused and cutnpaste is kept to a minimum, added testcode

--- projects/lostengine/resources/lost/resources/glsl/rectFrame.fs
+++ projects/lostengine/resources/lost/resources/glsl/rectFrame.fs
@@ -3,14 +3,9 @@ uniform vec2 size;
 varying vec2 tc0; 
 uniform float width;
 
+#import "lost/resources/glsl/rectFrame.fsp"
+
 void main(void)
 { 
-  vec2 cp = tc0*size;
-  float top = 1.0-step(width, size.y - cp.y);
-  float bottom = 1.0-step(width, cp.y);
-  float left = 1.0-step(width, cp.x);
-  float right = 1.0-step(width, size.x - cp.x);
-  float res = clamp(top+bottom+left+right, 0.0, 1.0);
-
-  gl_FragColor = color*res;
+  gl_FragColor = color*rectFrame(tc0, size, width);
 }
--- projects/lostengine/resources/lost/resources/glsl/rectGradient.vs
+++ projects/lostengine/resources/lost/resources/glsl/rectGradient.vs
@@ -3,11 +3,10 @@
 attribute vec2 position;
 attribute vec2 texcoord0;
 
-varying vec2 tc0; 
+varying vec2 tc0;
 
 void main(void)
 {
   tc0 = texcoord0;
   gl_Position = lftransform(vec4(position,0.0, 1.0)); 
 }
-
