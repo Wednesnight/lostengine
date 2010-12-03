d4fba471caa47d383339613a8b66979f30fb52ef
lobotony <lobotony666@yahoo.de>
2010-12-03 01:54:20 +0000

    cleaned up log messages, removed testtext that was drawn without layer, fixed stupid bug in FontManager which made it ignore the font size for cached fonts, implemented Text layer so it actually renders and aligns correctly, shadow is still missing, made test scene a bit prettier

--- sandbox/rrshadertest.tasklet/scene2.lua
+++ sandbox/rrshadertest.tasklet/scene2.lua
@@ -6,7 +6,6 @@ dcl.rg:Node
   dcl.rg:Clear { mask = gl.GL_COLOR_BUFFER_BIT },
   dcl.rg:DepthTest{false},
   dcl.rg:Camera2D { viewport = Rect(0,0,config.window.width, config.window.height) },    
-  dcl.rg:Draw{mesh=renderedText}
 --  meshFactory:comboRectNode(blue, red, Rect(left,390,200,200), 27, 2, {tl=false, tr=true, bl=false, br=true}, {left=false}),
 --  meshFactory:iqcreateComboRectNode(yellow, white, Rect(right,390,200,200), 27, 2),  
 }
