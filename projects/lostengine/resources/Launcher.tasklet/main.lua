998dd47fd5b713c1981071090afe26d0fdf9fe2d
lobotony <lobotony666@yahoo.de>
2010-12-05 16:18:58 +0000

    trying different formatting

--- projects/lostengine/resources/Launcher.tasklet/main.lua
+++ projects/lostengine/resources/Launcher.tasklet/main.lua
@@ -7,6 +7,11 @@ local Color = lost.common.Color
 
 function startup()
   local ui = lost.guiro.ui()
+  
+  local r = lost.guiro.layer.Rect
+  local rr = lost.guiro.layer.RoundedRect
+  local t = lost.guiro.layer.Text
+  
   ui:add
   {
     lost.guiro.view.View
@@ -21,69 +26,18 @@ function startup()
       },
       sublayers = 
       {
-        lost.guiro.layer.Rect
-        {
-          bounds = {0,0,"1","1"},
-          id = "whiteBackground",
-          filled = true,
-          color = Color(1,1,1)
-        },
-        lost.guiro.layer.RoundedRect
-        {
-          id = "leftBox",
-          bounds = {{"left",10}, {"bottom",10}, {".5",-15},{"1",-20}},
-          color = Color(1,.5,.2),
-          radius = 8,
-          filled = true
-        },
-        lost.guiro.layer.RoundedRect
-        {
-          id = "rightBox",
-          bounds = {{"right",-10}, {"bottom",10}, {".5",-15},{"1",-20}},
-          color = Color(1,.2,.2),
-          radius = 8,
-          filled = true
-        },
-        lost.guiro.layer.RoundedRect
-        {
-          id = "centerTextBg",
-          bounds = {"center", "center", ".7", ".4"},
-          color = Color(1, 1, 1,1),
-          radius = 8,
-          filled = true
-        },
-        lost.guiro.layer.Text
-        {
-          id="dropMessage",
-          bounds = {"center", "center", ".6", ".4"},
-          font = {"Grinched", 28},
-          text = "Drop tasklet directory here!",
-          color = Color(0,0,0)          
-        },
-
-        lost.guiro.layer.RoundedRect
-        {
-          id = "leftCutout",
-          bounds = {"left", "center", ".10", ".4"},
-          color = Color(1, 1, 1),
-          radius = 8,
-          filled = true,
-        },
-        lost.guiro.layer.RoundedRect
-        {
-          id = "rightCutout",
-          bounds = {"right", "center", ".10", ".4"},
-          color = Color(1, 1, 1),
-          radius = 8,
-          filled = true,
-        },
-
+        r{bounds={0,0,"1","1"},id="whiteBackground",filled=true,color=Color(1,1,1)},
+        rr{id="leftBox",bounds={{"left",10}, {"bottom",10}, {".5",-15},{"1",-20}},color=Color(1,.5,.2),radius=8,filled=true},
+        rr{id="rightBox",bounds={{"right",-10}, {"bottom",10}, {".5",-15},{"1",-20}},color=Color(1,.2,.2),radius=8,filled=true},
+        rr{id="centerTextBg",bounds={"center", "center", ".7", ".4"},color=Color(1, 1, 1,1),radius=8,filled=true},
+        t{id="dropMessage",bounds={"center", "center", ".6", ".4"},font={"Grinched", 28},text="Drop tasklet directory here!",color=Color(0,0,0)},
+        rr{id="leftCutout",bounds={"left", "center", ".10", ".4"},color=Color(1, 1, 1),radius=8,filled=true},
+        rr{id="rightCutout",bounds={"right", "center", ".10", ".4"},color=Color(1, 1, 1),radius=8,filled=true},
       }
     }
   }
   tasklet.eventDispatcher:addEventListener(lost.application.KeyEvent.KEY_DOWN, keyHandler)
 end
-
 
 function keyHandler(event)
   if event.key == lost.application.K_ESCAPE then
