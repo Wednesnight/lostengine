4eb50f64886088998a51e9ed3ead89d7d3bfff63
lobotony <lobotony666@yahoo.de>
2010-12-06 16:45:27 +0000

    got rid of Singletons.lua because it caused too many cyclic dependency problems, moved singlton accessors to corresponding class files, fixed a layout bug in view where rootLayer of a view was always placed at 0/0, added additional Label style for testing

--- projects/lostengine/resources/lost/guiro/themes/Pebble.lua
+++ projects/lostengine/resources/lost/guiro/themes/Pebble.lua
@@ -4,6 +4,7 @@ require("lost.common.Class")
 require("lost.guiro.Theme")
 
 require("lost.guiro.layer.Text")
+require("lost.guiro.layer.RoundedRect")
 
 lost.common.Class "lost.guiro.themes.Pebble" "lost.guiro.Theme" {}
 
@@ -13,7 +14,11 @@ function Pebble:constructor()
   lost.guiro.Theme.constructor(self)
   self.name = "pebble"
   self:addStyle("lost.guiro.view.Label", "default", function(target, args) self:labelDefault(target, args) end)
+  self:addStyle("lost.guiro.view.Label", "round", function(target, args) self:labelRound(target, args) end)
+  self:addStyle("lost.guiro.view.Label", "roundFramed", function(target, args) self:labelRoundFramed(target, args) end)
   self:addStyle("lost.guiro.view.Button", "default", function(target, args) self:buttonGray(target, args) end)
+  self:addStyle("lost.guiro.view.View", "gray", function(target, args) self:viewGray(target, args) end)
+--  self:addStyle("lost.guiro.view.View", "default", function(target, args) self:viewGray(target, args) end)
 end
 
 function Pebble:labelDefault(target, args)
@@ -25,7 +30,59 @@ function Pebble:labelDefault(target, args)
                             })
 end
 
+function Pebble:labelRound(target, args)
+  target.layer:addSublayer(lost.guiro.layer.RoundedRect
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(1,1,1),
+                              radius=8,
+                              filled = true
+                            })
+  target.layer:addSublayer(lost.guiro.layer.Text
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(0,0,0),
+                              font = {"Vera", 12}
+                            })
+end
+
+function Pebble:labelRoundFramed(target, args)
+  target.layer:addSublayer(lost.guiro.layer.RoundedRect
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(1,1,1),
+                              radius=8,
+                              filled = true
+                            })
+  target.layer:addSublayer(lost.guiro.layer.RoundedRect
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(0,0,0),
+                              radius=8,
+                              filled = false,
+                              width=1
+                            })
+  target.layer:addSublayer(lost.guiro.layer.Text
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(0,0,0),
+                              font = {"Vera", 12}
+                            })
+end
+
+
 function Pebble:buttonGray(target,args)
   log.debug("------------ YAY!")
   local b = lost.guiro.view.Button
+  
+end
+
+function Pebble:viewGray(target, args)
+  log.debug("-------- view gray")
+  target.layer:addSublayer(lost.guiro.layer.Rect
+                            {
+                              bounds={0,0,"1","1"},
+                              color = Color(.9294,.9294,.9294),
+                              filled = true
+                            })  
 end
