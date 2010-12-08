6021b28acd3edff814ff7282f608cc0b312c8cb4
lobotony <lobotony666@yahoo.de>
2010-12-08 18:21:47 +0000

    button toggle mode now works, added test code, added empty Disc file

--- projects/Guiroscope.tasklet/main.lua
+++ projects/Guiroscope.tasklet/main.lua
@@ -43,14 +43,19 @@ function startup()
         {
           lost.guiro.view.Label{bounds={200,200,100,30},text="hello",style="roundFramed"},
           lost.guiro.view.Button{id="ugly",mode="sticky",bounds={50,0,100,30},title="Button",},
-          lost.guiro.view.Button{id="regular",mode="toggle",bounds={50,{"top",-10},154,50},style="rounded",size="regular", title="Regular"},
+          lost.guiro.view.Button{id="regular",mode="normal",bounds={50,{"top",-10},154,50},style="rounded",size="regular", title="Regular"},
           lost.guiro.view.Button{id="small",mode="sticky",bounds={50,{"top",-30},154,70},style="rounded",size="small", title="Small"},
-          lost.guiro.view.Button{id="mini",bounds={50,{"top",-50},154,90},style="rounded",size="mini", title="Mini"}
+          lost.guiro.view.Button{id="mini",bounds={50,{"top",-50},154,90},style="rounded",size="mini", title="Mini"},
+          lost.guiro.view.Button{id="regular",mode="toggle",bounds={50,{"top",-10},154,50},style="roundedToggle",size="regular",title="Toggle"},
         }
       }
     }
   }
   ui:printSubviews()
+end
+
+function update()
+--  ui.layer.renderNode:print()
 end
 
 function keyHandler(event)
--- projects/lostengine/resources/lost/guiro/themes/Pebble.lua
+++ projects/lostengine/resources/lost/guiro/themes/Pebble.lua
@@ -22,6 +22,7 @@ function Pebble:constructor()
   self:addStyle("lost.guiro.view.Label", "roundFramed", function(target, args) self:labelRoundFramed(target, args) end)
   self:addStyle("lost.guiro.view.Button", "default", function(target, args) self:buttonGray(target, args) end)
   self:addStyle("lost.guiro.view.Button", "rounded", function(target, args) self:buttonRounded(target, args) end)
+  self:addStyle("lost.guiro.view.Button", "roundedToggle", function(target, args) self:buttonRoundedToggle(target, args) end)
   self:addStyle("lost.guiro.view.View", "gray", function(target, args) self:viewGray(target, args) end)
   self:addStyle("lost.guiro.view.View", "toolbar", function(target, args) self:viewToolbar(target, args) end)
   self:addStyle("lost.guiro.view.UserInterface", "default", function(target, args) self:viewGray(target, args) end)
@@ -89,6 +90,59 @@ function Pebble:buttonRounded(target,args)
   target.titleColors[b.STATE_NORMAL] = Color(1,0,0)
   target.titleColors[b.STATE_HOVER] = Color(0,1,0)
   target.titleColors[b.STATE_PUSHED] = Color(1,1,0)
+  target.titleColors[b.STATE_DISABLED] = Color(1,1,1)
+end
+
+function Pebble:buttonRoundedToggle(target,args)
+  local l = lost.guiro.layer.Layer
+  local rr = lost.guiro.layer.RoundedRect
+
+  local b = lost.guiro.view.Button
+  local size = args.size or "small"
+  -- target bounds must have been set by now so we can modify height
+  target._bounds.height = lost.guiro.Bounds.decodeEntry(4,self.buttonRoundedHeight[size])
+  local r = self.buttonRoundedHeight[size]/2 -- rounded rect radius
+  local normal = l{id="normal",sublayers={rr{bounds={0,0,"1","1"},gradient="segLightGrey",filled=true,radius=r},
+                              rr{bounds={0,0,"1","1"},gradient="segGreyFrame",filled=false,radius=r}}
+                             }
+--[[ local hover = l{id="hover",sublayers={rr{bounds={0,0,"1","1"},color=Color(1,1,0),filled=true,radius=r},
+                             rr{bounds={0,0,"1","1"},color=self.buttonRoundedFrameCol,filled=false,radius=r}}
+                            }]]
+  local pushed = l{id="pushed",sublayers={rr{bounds={0,0,"1","1"},gradient="rrbg2",filled=true,radius=r},
+                              rr{bounds={0,0,"1","1"},color=self.buttonRoundedFrameCol,filled=false,radius=r}}
+                             }
+
+  local normal2 = l{id="normal2",sublayers={rr{bounds={0,0,"1","1"},gradient="segBlue",filled=true,radius=r},
+                               rr{bounds={0,0,"1","1"},gradient="segBlueFrame",filled=false,radius=r}}
+                              }
+--[[  local hover2 = l{id="hover2",sublayers={rr{bounds={0,0,"1","1"},color=Color(1,1,.5),filled=true,radius=r},
+                              rr{bounds={0,0,"1","1"},color=self.buttonRoundedFrameCol,filled=false,radius=r}}
+                             }]]
+  local pushed2 = l{id="pushed2",sublayers={rr{bounds={0,0,"1","1"},gradient="segBlue",color=Color(.8,.8,.8),filled=true,radius=r},
+                               rr{bounds={0,0,"1","1"},gradient="segBlueFrame",filled=false,radius=r}}
+                              }
+
+  local text = lost.guiro.layer.Text{bounds={0,0,"1","1"},font=self.buttonRoundedFonts[size],color=Color(0,0,0)}
+  target.layer:addSublayer(normal)
+  target.layer:addSublayer(pushed)
+--  target.layer:addSublayer(hover)
+  target.layer:addSublayer(normal2)
+  target.layer:addSublayer(pushed2)
+--  target.layer:addSublayer(hover2)
+  target.layer:addSublayer(text)
+  target.textLayer = text
+  target.backgrounds[b.STATE_NORMAL] = normal
+  target.backgrounds[b.STATE_PUSHED] = pushed
+--  target.backgrounds[b.STATE_HOVER] = hover
+  target.backgrounds[b.STATE_NORMAL2] = normal2
+  target.backgrounds[b.STATE_PUSHED2] = pushed2
+  --  target.backgrounds[b.STATE_HOVER2] = hover2
+  target.titleColors[b.STATE_NORMAL] = Color(1,0,0)
+  target.titleColors[b.STATE_HOVER] = Color(0,1,0)
+  target.titleColors[b.STATE_PUSHED] = Color(1,1,0)
+  target.titleColors[b.STATE_NORMAL2] = Color(.8,.8,.8)
+  target.titleColors[b.STATE_HOVER2] = Color(1,1,1)
+  target.titleColors[b.STATE_PUSHED2] = Color(.8,.8,.8)
   target.titleColors[b.STATE_DISABLED] = Color(1,1,1)
 end
 
