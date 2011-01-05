71781670f84feaeca9c8b29f49967157add759ff
lobotony <lobotony666@yahoo.de>
2011-01-05 09:10:47 +0000

    #229 RadioGroup now works, tweaked radioCandy Button style, removed offset from disc again, still looks weird, added ´more RadioGroup tests

--- projects/Guiroscope.tasklet/views_button.lua
+++ projects/Guiroscope.tasklet/views_button.lua
@@ -70,15 +70,17 @@ return lost.guiro.view.View
         lost.guiro.view.Button
         {
           style="radioCandy",
-          size="regular",
+          size="mini",
           title = "Radio 1",
+          id="rg1",
           bounds = {50,"top",100,30}
         },
         lost.guiro.view.Button
         {
           style="radioCandy",
-          size="small",
+          size="mini",
           title = "Radio 2",
+          id="rg2",
           bounds = {50,{"top",-25},100,30}
         },
         lost.guiro.view.Button
@@ -86,9 +88,75 @@ return lost.guiro.view.View
           style="radioCandy",
           size="mini",
           title = "Radio 3",
+          id="rg3",
+          bounds = {50,{"top",-50},100,30}
+        },        
+      }
+    },
+    lost.guiro.view.RadioGroup
+    {
+      bounds = {400,200,200,200},
+      layout = lost.guiro.layout.Vertical{halign="center",valign="center",spacing = 4},
+      subviews = 
+      {
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="small",
+          title = "Radio 1",
+          id="rg1",
+          bounds = {50,"top",100,30}
+        },
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="small",
+          title = "Radio 2",
+          id="rg2",
+          bounds = {50,{"top",-25},100,30}
+        },
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="small",
+          title = "Radio 3",
+          id="rg3",
+          bounds = {50,{"top",-50},100,30}
+        },        
+      }
+    },
+    lost.guiro.view.RadioGroup
+    {
+      bounds = {600,200,200,200},
+      layout = lost.guiro.layout.Vertical{halign="center",valign="center",spacing = 4},
+      subviews = 
+      {
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="regular",
+          title = "Radio 1",
+          id="rg1",
+          bounds = {50,"top",100,30}
+        },
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="regular",
+          title = "Radio 2",
+          id="rg2",
+          bounds = {50,{"top",-25},100,30}
+        },
+        lost.guiro.view.Button
+        {
+          style="radioCandy",
+          size="regular",
+          title = "Radio 3",
+          id="rg3",
           bounds = {50,{"top",-50},100,30}
         },        
       }
     } 
+    
   }
 }
