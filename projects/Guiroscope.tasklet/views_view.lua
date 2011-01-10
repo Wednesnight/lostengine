f351b4237548c2a7de1b00fd9547ef0cc4963eb9
lobotony <lobotony666@yahoo.de>
2011-01-10 12:43:20 +0000

    #234 TabView works

--- projects/Guiroscope.tasklet/views_view.lua
+++ projects/Guiroscope.tasklet/views_view.lua
@@ -1,4 +1,5 @@
 require("lost.guiro.view.Box")
+require("lost.guiro.view.TabView")
 
 local Color = lost.common.Color
 
@@ -23,8 +24,37 @@ return lost.guiro.view.View
         {
           bounds={0,"top","1",30},
           title="Testing",
-        }
+        },        
       }
-    }
+    },
+    lost.guiro.view.TabView
+    {
+      style="candyRoundedRecess",
+      bounds={250,20,300,300},
+      items={{"view1",lost.guiro.view.Button{title="Hello 1"}},
+             {"view2",lost.guiro.view.Button{title="Hello 2",bounds={0,"top",50,50}}},
+             {"view3",lost.guiro.view.Button{title="Hello 3",bounds={"right","center",80,80}}}
+             }
+    },
+    lost.guiro.view.TabView
+    {
+      style="candyRecess",
+      bounds={250,350,300,300},
+      items={{"view1",lost.guiro.view.Button{title="Hello 1"}},
+             {"view2",lost.guiro.view.Button{title="Hello 2",bounds={0,"top",50,50}}},
+             {"view3",lost.guiro.view.Button{title="Hello 3",bounds={"right","center",80,80}}}
+             }
+    },
+    lost.guiro.view.TabView
+    {
+      style="square",
+      bounds={650,20,200,300},
+      items={{"view1",lost.guiro.view.Button{title="Hello 1"}},
+             {"view2",lost.guiro.view.Button{title="Hello 2",bounds={0,"top",50,50}}},
+             {"view3",lost.guiro.view.Button{title="Hello 3",bounds={"right","center",80,80}}}
+             }
+    }    
+
+
   }
 }
--- projects/lostengine/resources/lost/guiro/view/TabView.lua
+++ projects/lostengine/resources/lost/guiro/view/TabView.lua
@@ -4,8 +4,59 @@ require("lost.guiro.view.View")
 
 lost.common.Class "lost.guiro.view.TabView" "lost.guiro.view.View" {}
 
+-- mandatory, set by theme:
+-- self.tabbarView
+-- self.contentView
+-- params:
+-- * items: table containing tables containing string/view pairs
+--     e.g. items = {{"first view", v1}, {"second view",v2}}
 function TabView:constructor(args)
-  lost.guiro.view.View.constructor(self, args)
+  -- remove items from args so they don't clash with tabbar 
   local t = args or {}
+  local items = t.items or {}
+  t.items = nil
+  lost.guiro.view.View.constructor(self, t)
+  self.tabbarItems = {}
+  self.tabViews = {}
+  for k,item in ipairs(items) do
+    table.insert(self.tabbarItems, item[1])
+    table.insert(self.tabViews, item[2])    
+  end
+  self.tabbarView._items = self.tabbarItems
+  self.tabbarView:rebuildButtons()
+  for k,view in ipairs(self.tabViews) do
+    self:addSubview(view)
+  end
+  self.selected = t.selected or 1
+  self:select(self.selected)
+  self.tabbarView:addEventListener("tabBarSelectionChanged",function(event) self:tabbarSelectionChanged(event) end)
 end
 
+function TabView:tabbarSelectionChanged(event)
+  self:select(event.target.selected)
+end
+
+function TabView:addSubview(view,forceSubview)
+  if self.contentView and not forceSubview then
+    self.contentView:addSubview(view)
+  else
+    lost.guiro.view.View.addSubview(self, view)
+  end
+end
+
+function TabView:hideAll()
+  for k,v in ipairs(self.tabViews) do
+    v:hidden(true)
+  end
+end
+
+function TabView:select(val)
+  self:hideAll()
+  self.tabViews[val]:hidden(false)
+  self.selected = val
+
+  local ev = lost.guiro.event.Event("tabViewSelectionChanged")
+  ev.bubbles = true
+  ev.target = self
+  self:dispatchEvent(ev)  
+end
