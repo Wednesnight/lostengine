8a62d3ad7a63943253f70dd4da671d52512d94e3
lobotony <lobotony666@yahoo.de>
2011-01-19 15:52:52 +0000

    #231, added proper setters/getters and constructors for both Text layer and Label view, enabled shadow for Launcher title

--- projects/lostengine/resources/Launcher.tasklet/main.lua
+++ projects/lostengine/resources/Launcher.tasklet/main.lua
@@ -1,4 +1,5 @@
 local Color = lost.common.Color
+local Vec2 = lost.math.Vec2
 
 function startup()  
   require("lost.guiro")
@@ -22,7 +23,14 @@ function startup()
         rr{id="leftBox",bounds={{"left",10}, {"bottom",10}, {".5",-15},{"1",-20}},color=Color(1,.5,.2),radius=8,filled=true},
         rr{id="rightBox",bounds={{"right",-10}, {"bottom",10}, {".5",-15},{"1",-20}},color=Color(1,.2,.2),radius=8,filled=true},
         rr{id="centerTextBg",bounds={"center", "center", ".7", ".4"},color=Color(1, 1, 1,1),radius=8,filled=true},
-        t{id="dropMessage",bounds={"center", "center", ".6", ".4"},font={"Grinched", 28},text="Drop tasklet directory here!",color=Color(0,0,0)},
+        t{id="dropMessage",
+          bounds={"center", "center", ".6", ".4"},
+          font={"Grinched", 28},
+          text="Drop tasklet directory here!",
+          color=Color(0,0,0),
+          shadow=true,
+          shadowOffset=Vec2(2,-2),
+          shadowColor=Color(0,0,0,.3)},
         rr{id="leftCutout",bounds={"left", "center", ".10", ".4"},color=Color(1, 1, 1),radius=8,filled=true},
         rr{id="rightCutout",bounds={"right", "center", ".10", ".4"},color=Color(1, 1, 1),radius=8,filled=true},
       }
--- projects/lostengine/resources/lost/guiro/view/Label.lua
+++ projects/lostengine/resources/lost/guiro/view/Label.lua
@@ -4,26 +4,22 @@ require("lost.guiro.view.View")
 
 lost.common.Class "lost.guiro.view.Label" "lost.guiro.view.View" {}
 
+local Color = lost.common.Color
+local Vec2 = lost.math.Vec2
+
 function Label:constructor(args)
   lost.guiro.view.View.constructor(self, args)
   local t = args or {}
   self.id = t.id or "label"
   self.textLayer = self.layer:recursiveFindById("text")
-  if t.font then
-    self.textLayer:font(tasklet.fontManager:getFont(t.font[1], t.font[2]))
-  end
-  if t.text then
-    self.textLayer:text(t.text)
-  end
-  if t.color then
-    self.textLayer:color(t.color)
-  end
-  if t.valign then
-    self.textLayer:valign(t.valign)
-  end
-  if t.halign then
-    self.textLayer:halign(t.halign)
-  end
+  self.textLayer:font(t.font or {"Vera", 12})
+  self.textLayer:text(t.text or "")
+  self.textLayer:color(t.color or Color(0,0,0))
+  self.textLayer:shadow(t.shadow or false)
+  self.textLayer:shadowOffset(t.shadowOffset or Vec2(1,1))
+  self.textLayer:shadowColor(t.shadowColor or Color(0,0,0,.3))
+  self.textLayer:valign(t.valign or "center")
+  self.textLayer:halign(t.halign or "center")
 end
 
 function Label:text(...)
@@ -37,4 +33,62 @@ function Label:text(...)
     end
   end
 end
+
+function Label:font(...)
+  if arg.n >=1 then
+    self.textLayer:font(arg[1])
+  else
+    return self.textLayer._font
+  end
+end
+
+function Label:color(...)
+  if arg.n >=1 then
+    self.textLayer:color(arg[1])
+  else
+    return self.textLayer:color()
+  end
+end
+
+function Label:valign(...)
+  if arg.n >=1 then
+    self.textLayer:valign(arg[1])
+  else
+    return self.textLayer:valign()
+  end
+end
+
+function Label:halign(...)
+  if arg.n >=1 then
+    self.textLayer:halign(arg[1])
+  else
+    return self.textLayer:halign()
+  end
+end
+
+function Label:shadow(...)
+  if arg.n >=1 then
+    self.textLayer:shadow(arg[1])
+  else
+    return self.textLayer:shadow()
+  end
+end
+
+function Label:shadowOffset(...)
+  if arg.n >=1 then
+    self.textLayer:shadowOffset(arg[1])
+  else
+    return self.textLayer:shadowOffset()
+  end
+end
+
+function Label:shadowColor(...)
+  if arg.n >=1 then
+    self.textLayer:shadowColor(arg[1])
+  else
+    return self.textLayer:shadowColor()
+  end
+end
+
+
 
