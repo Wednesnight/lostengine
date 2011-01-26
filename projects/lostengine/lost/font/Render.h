62e980854c5dffd7a4daa83f69624f1ee3f940b7
lobotony <lobotony666@yahoo.de>
2011-01-26 17:56:10 +0000

    added ranged render function

--- projects/lostengine/lost/font/Render.h
+++ projects/lostengine/lost/font/Render.h
@@ -12,6 +12,7 @@ RenderedTextPtr render(const std::string & inText, const FontPtr& font);
 RenderedTextPtr render(const fhtagn::text::utf32_string& inText, const FontPtr& font);   
 void render(const std::string & inText, const FontPtr& font, const RenderedTextPtr& target);
 void render(const fhtagn::text::utf32_string& inText, const FontPtr& font, const RenderedTextPtr& target);
+void render(const fhtagn::text::utf32_string& inText, uint32_t begin, uint32_t end, const FontPtr& font, const RenderedTextPtr& target);
 }
 }
 
