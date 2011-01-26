464348a08b5cac0eaa7c04ef961b62bd107b716c
lobotony <lobotony666@yahoo.de>
2011-01-26 04:13:54 +0000

    work in progress

---
+++ sandbox/ttest.tasklet/config.lua
@@ -0,0 +1,16 @@
+return {
+  tasklet = {
+    name = "ttest",
+    waitForEvents = true
+  },
+  window = {
+    title = "ttest",
+    x = 50,
+    y = 50,
+    width = 400,
+    height = 400
+  },
+  gl ={
+    vsync = false
+  }
+}
