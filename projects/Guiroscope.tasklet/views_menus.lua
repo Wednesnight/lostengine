f4d7c2b7fcfc8d129a180746b00a630219872c7f
lobotony <lobotony666@yahoo.de>
2011-01-29 18:39:33 +0000

    added empty style functions, menubarback gradienta#

--- projects/Guiroscope.tasklet/views_menus.lua
+++ projects/Guiroscope.tasklet/views_menus.lua
@@ -1,8 +1,32 @@
-local Color = lost.common.Color
-
 return lost.guiro.view.View
 {
   subviews=
   {
+      lost.guiro.view.MenuBar
+      {
+        bounds={0,"top","1",30},
+        items={
+          {"Menu 1", lost.guiro.view.Menu{
+            items={
+              {title="m1 First"},
+              {title="m1 Second"},
+              "-",
+              {title="m1 Third"},              
+              {title="m1 Fourth"},              
+            }
+          }},
+          {"Menu 2", lost.guiro.view.Menu{
+            items={
+              {title="m2 First"},
+              {title="m2 Second"},
+              "-",
+              {title="m2 Third"},              
+              {title="m2 Fourth"},              
+              "-",
+              {title="m2 Fifth"},              
+            }
+          }}
+        }
+      },
   }
 }
