8df201cb9059b176995f4557790b786a8b024129
jomanto <jomanto@gmx.net>
2011-02-06 02:38:59 +0000

    update gyp with current sources, added resources script

---
+++ projects/lostengine/copy_resources.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+exists=$(ls -l "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents" | grep -xi 'd.*Resources' | awk '{print $NF}')
+if [[ "$exists" ]]
+then
+  rm -fR "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents/Resources/lost"
+  rm -fR "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents/Resources/Launcher.tasklet"
+else
+  mkdir "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents/Resources"
+fi
+echo cp -Rf resources/* "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents/Resources/"
+cp -Rf resources/* "${BUILD_DIR}/${CONFIGURATION}/${PRODUCT_NAME}.app/Contents/Resources/"
