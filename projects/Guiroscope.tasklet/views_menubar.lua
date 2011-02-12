0acdfd1676541538d368cb7825a430cedb57bd94
lobotony <lobotony666@yahoo.de>
2011-02-12 11:21:28 +0000

    added test popupbutton, changed global menuItemSelected handler so event.target can also be a popupbutton

--- projects/Guiroscope.tasklet/views_menubar.lua
+++ projects/Guiroscope.tasklet/views_menubar.lua
@@ -12,8 +12,11 @@ local function checkFunction(event)
   for k,v in ipairs(event.indexPath) do
     log.debug(v)
   end
-  local mi = event.target:menuItemForIndexPath(event.indexPath)
-  mi:checked(not mi:checked())
+  -- target can be a popupbutton so we need to check
+  if event.target.menuItemForIndexPath then
+    local mi = event.target:menuItemForIndexPath(event.indexPath)
+    mi:checked(not mi:checked())
+  end
 end
 
 -- FIXME: menuItemSelected events only through ui
--- projects/Guiroscope.tasklet/views_popupbutton.lua
+++ projects/Guiroscope.tasklet/views_popupbutton.lua
@@ -6,8 +6,22 @@ return lost.guiro.view.View
   {
     lost.guiro.view.PopUpButton
     {
+      listeners=
+      {
+        menuItemSelected=function(event) log.debug("mi selected") end
+      },
       bounds={"center","center",100,30},
-      title = "Colors"
+      title = "Colors",
+      menu={
+        items=
+        {
+          {title="m1 First"},
+          {title="m1 Second"},
+          "-",
+          {title="m1 Third"},              
+          {title="m1 Fourth"},              
+        }
+      },
     }
   }
 }
