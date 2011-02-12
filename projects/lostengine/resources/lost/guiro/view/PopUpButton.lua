3895d5fbb1a5b6d25e0d0a867d52d796bf2e23ed
lobotony <lobotony666@yahoo.de>
2011-02-12 11:20:20 +0000

    popupbutton works, needs appropriate styling and sizes now

--- projects/lostengine/resources/lost/guiro/view/PopUpButton.lua
+++ projects/lostengine/resources/lost/guiro/view/PopUpButton.lua
@@ -2,8 +2,51 @@ module("lost.guiro.view", package.seeall)
 
 lost.common.Class "lost.guiro.view.PopUpButton" "lost.guiro.view.Button" {}
 
+-- must be set from style:
+-- * menuTheme
+-- * menuStyle
 function PopUpButton:constructor(args)
   lost.guiro.view.Button.constructor(self, args)
   local t = args or {}
   self.id = t.id or "popupbutton"
+  local ms = {}
+  ms.theme = self.menuTheme
+  ms.style = self.menuStyle
+  local incoming = t.menu or {}
+  for k,v in pairs(incoming) do 
+    ms[k] = v
+  end
+  self.menu = lost.guiro.view.Menu(ms)
+  self:addEventListener("buttonClick", function(event) self:buttonClick(event) end)
+end
+
+function PopUpButton:items(it)
+  self.menu:items(it)
+end
+
+function PopUpButton:buttonClick(event)
+  self.menu:x(self.rect.x)
+  self.menu:y(self.rect.y+self.rect.height-self.menu.rect.height)
+  self.menu.delegate = self
+  self.menu:open()
+end
+
+-- Menu delegate methods
+
+function PopUpButton:menuItemSelected(menu, indexPath)
+  self.menu:close()
+  local mi = self.menu:menuItemForIndexPath(indexPath)
+  self:title(mi:title())
+  self:pushed(false)
+
+  local event = lost.guiro.event.Event("menuItemSelected")
+  event.bubbles = true
+  event.target = self
+  event.indexPath = indexPath
+  self:dispatchEvent(event)    
+end
+
+function PopUpButton:menuExternalCloseRequest(menu, event)
+  self.menu:close()
+  self:pushed(false)
 end
