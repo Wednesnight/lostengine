9a12c19d6c5095d3d24e8139e9d0c4e266bf3f4f
lobotony <lobotony666@yahoo.de>
2011-02-14 17:51:01 +0000

    added select method and selection constructor parameter to PopUpButton, now automatically manages checkmark tweaked image layout in style

--- projects/Guiroscope.tasklet/views_popupbutton.lua
+++ projects/Guiroscope.tasklet/views_popupbutton.lua
@@ -9,9 +9,8 @@ return lost.guiro.view.View
   layout=lost.guiro.layout.Vertical{valign="center", halign="center",spacing=10},
   subviews=
   {
-    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="regular",title = "Regular",menu=require("pubmenu1")},
-    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="small",title = "Small",menu=require("pubmenu1")},
-    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="mini",title = "Mini",menu=require("pubmenu1")},
-    lost.guiro.view.Button{bounds={0,0,100,20},style="rounded"}
+    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="regular",title = "Regular",menu=require("pubmenu1"),selected={1}},
+    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="small",title = "Small",menu=require("pubmenu1"),selected={2}},
+    lost.guiro.view.PopUpButton{bounds={0,0,100,30},size="mini",title = "Mini",menu=require("pubmenu1"),selected={3}},
   }
 }
