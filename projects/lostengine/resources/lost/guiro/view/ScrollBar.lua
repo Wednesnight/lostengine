7b63460c8393f3153882fb89d6615dc9f506cfa3
lobotony <lobotony666@yahoo.de>
2011-02-16 18:36:13 +0000

    scrollbar work in progres

--- projects/lostengine/resources/lost/guiro/view/ScrollBar.lua
+++ projects/lostengine/resources/lost/guiro/view/ScrollBar.lua
@@ -1,14 +1,21 @@
 module("lost.guiro.view", package.seeall)
 
-lost.common.Class "lost.guiro.view.ScrollBar" "lost.guiro.view.Slider" {}
+lost.common.Class "lost.guiro.view.ScrollBar" "lost.guiro.view.View" {}
 
 -- * handleSize: [0..1] in percent of the current grooveSize
 -- * handleLayer: must be inside grooveLayer
 -- * grooveLayer: background of handle, contains handle
 -- * upButton
 -- * downButton
+--
+-- controls, set by theme
+-- * slider
+-- * incButton
+-- * decButton
+--
 function ScrollBar:constructor(args)
-  lost.guiro.view.Slider.constructor(self, args)
+  lost.guiro.view.View.constructor(self, args)
   local t = args or {}
   self.id = t.id or "scrollbar"
+  self:needsLayout()
 end
