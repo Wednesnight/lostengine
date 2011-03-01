bbf60a2258d55348b32f98f45c0f7f4ede19bd2a
lobotony <lobotony666@yahoo.de>
2011-03-01 18:59:59 +0000

    added visibleRange, handle now resizes according to range, paging works by clicking in track of slider, added test slider to modify visibleRange in sbtest

--- projects/lostengine/resources/lost/guiro/view/ScrollBar.lua
+++ projects/lostengine/resources/lost/guiro/view/ScrollBar.lua
@@ -2,27 +2,70 @@ module("lost.guiro.view", package.seeall)
 
 lost.common.Class "lost.guiro.view.ScrollBar" "lost.guiro.view.View" {}
 
--- * handleSize: [0..1] in percent of the current grooveSize
--- * handleLayer: must be inside grooveLayer
--- * grooveLayer: background of handle, contains handle
--- * upButton
--- * downButton
---
 -- controls, set by theme
 -- * slider
 -- * incButton
 -- * decButton
+-- params set by theme
+-- * buttonSize: required for slider handle size recalculation depending on visibleRange
 --
 function ScrollBar:constructor(args)
   lost.guiro.view.View.constructor(self, args)
   local t = args or {}
   self.id = t.id or "scrollbar"
+  self.mode = t.mode or "horizontal"
   self:needsLayout()
   
   self.incButton:addEventListener("mouseDown", function(event) self:incButtonDown(event) end)
   self.incButton:addEventListener("mouseUp", function(event) self:incButtonUp(event) end)
   self.decButton:addEventListener("mouseDown", function(event) self:decButtonDown(event) end)
   self.decButton:addEventListener("mouseUp", function(event) self:decButtonUp(event) end)
+  self.slider:addEventListener("mouseDown", function(event) self:sliderMouseDown(event) end)
+  self.slider:addEventListener("mouseUp", function(event) self:sliderMouseUp(event) end)
+end
+
+function ScrollBar:updateLayout()
+  lost.guiro.view.View.updateLayout(self)
+  
+  local hs = 0
+  if self.mode == "horizontal" then
+    hs = self.rect.width - 2*self.buttonSize
+  else
+    hs = self.rect.height - 2*self.buttonSize
+  end  
+  self.slider:handleSize(hs*self._visibleRange)
+end
+
+function ScrollBar:visibleRange(...)
+  if arg.n >= 1 then 
+    self._visibleRange = arg[1]
+    self:needsLayout()
+  else
+    return self._visibleRange
+  end
+end
+
+function ScrollBar:stepSize(...)
+  if arg.n >= 1 then
+    self._stepSize = arg[1]
+  else
+    return self._stepSize
+  end
+end
+
+function ScrollBar:sliderMouseDown(event)
+  log.debug("slider "..event.type.." inHandle:"..tostring(self.slider:handleContainsPoint(event.pos)).." over:"..tostring(self.slider:pointOverHandle(event.pos)).." under:"..tostring(self.slider:pointUnderHandle(event.pos)))
+  if not self.slider:handleContainsPoint(event.pos) then
+    if self.slider:pointOverHandle(event.pos) then
+      self.slider:value(self.slider:value()+self._visibleRange)
+    elseif self.slider:pointUnderHandle(event.pos) then
+      self.slider:value(self.slider:value()-self._visibleRange)
+    end
+  end
+end
+
+function ScrollBar:sliderMouseUp(event)
+  log.debug("slider "..event.type.." inHandle:"..tostring(self.slider:handleContainsPoint(event.pos)).." over:"..tostring(self.slider:pointOverHandle(event.pos)).." under:"..tostring(self.slider:pointUnderHandle(event.pos)))
 end
 
 function ScrollBar:incButtonDown(event)
--- sandbox/sbtest.tasklet/main.lua
+++ sandbox/sbtest.tasklet/main.lua
@@ -6,7 +6,6 @@ function startup()
   require("lost/guiro")
   lost.guiro.ui():add
   {
---    lost.guiro.view.View{style="gray"},
     lost.guiro.view.View
     {
       bounds={"center","center",{"1",-margin},{"1",-margin}},
@@ -14,16 +13,32 @@ function startup()
       {
         lost.guiro.view.ScrollBar
         {
+          id="vsb",
           size=sbsize,
           mode="vertical",
           bounds={"right",scrollBarSize,scrollBarSize,{"1",-scrollBarSize}},
         },
         lost.guiro.view.ScrollBar
         {
+          id="hsb",
           size=sbsize,
           mode="horizontal",
           bounds={0,0,{"1",-scrollBarSize},scrollBarSize},
         },
+        lost.guiro.view.Slider
+        {
+          mode="horizontal",
+          bounds={"center","center",200,20},
+          min=0,
+          max=1,
+          listeners=
+          {
+            valueChanged = function(event)
+              lost.guiro.ui()("view")("vsb"):visibleRange(event.target:value())
+              lost.guiro.ui()("view")("hsb"):visibleRange(event.target:value())
+            end
+          }
+        }
       }
     }
   }
