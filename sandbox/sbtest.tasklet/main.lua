91b733eda479765c55b16db5ef7215bf72027c6d
lobotony <lobotony666@yahoo.de>
2011-03-07 16:38:49 +0000

    refactored for Guiroscope integration

--- sandbox/sbtest.tasklet/main.lua
+++ sandbox/sbtest.tasklet/main.lua
@@ -1,17 +1,12 @@
-local scrollBarSize = 15
-local sbsize= "regular"
-cellsize=50
-cellspacing=4
-gridWidth=4*cellsize+3*cellspacing
-gridHeight=5*cellsize+4*cellspacing
-
-local bmp = lost.bitmap.Bitmap.create(tasklet.loader:load("img/cancel.png"))
-bmp:premultiplyAlpha()
+svc_cellsize=50
+svc_cellspacing=4
+svc_gridWidth=4*svc_cellsize+3*svc_cellspacing
+svc_gridHeight=5*svc_cellsize+4*svc_cellspacing
 
 local Vec2 = lost.math.Vec2
 
-local margin = 0
-local blockSpacing = 2
+local svc_margin = 0
+local svc_blockSpacing = 2
 
 function startup()
   require("lost/guiro")
@@ -19,14 +14,14 @@ function startup()
   {
     lost.guiro.view.View
     {
-      bounds={margin,margin,{"1",-2*margin},{"1",-2*margin}},
+      bounds={svc_margin,svc_margin,{"1",-2*svc_margin},{"1",-2*svc_margin}},
       subviews=
       {
         lost.guiro.view.ScrollView
         {
           size="regular",
-          bounds={"left",0,{".33333",-blockSpacing},"1"},
-          contentSize=Vec2(gridWidth,gridHeight),
+          bounds={"left",0,{".33333",-svc_blockSpacing},"1"},
+          contentSize=Vec2(svc_gridWidth,svc_gridHeight),
           hasVerticalScrollbar = true,
           hasHorizontalScrollbar = true,          
           subviews=
@@ -37,8 +32,8 @@ function startup()
         lost.guiro.view.ScrollView
         {
           size="small",
-          bounds={"center",0,{".33333",-blockSpacing},"1"},
-          contentSize=Vec2(gridWidth,gridHeight),
+          bounds={"center",0,{".33333",-svc_blockSpacing},"1"},
+          contentSize=Vec2(svc_gridWidth,svc_gridHeight),
           hasVerticalScrollbar = true,
           hasHorizontalScrollbar = false,          
           subviews=
@@ -49,8 +44,8 @@ function startup()
         lost.guiro.view.ScrollView
         {
           size="mini",
-          bounds={"right",0,{".33333",-blockSpacing},"1"},
-          contentSize=Vec2(gridWidth,gridHeight),
+          bounds={"right",0,{".33333",-svc_blockSpacing},"1"},
+          contentSize=Vec2(svc_gridWidth,svc_gridHeight),
           hasVerticalScrollbar = false,
           hasHorizontalScrollbar = true,          
           subviews=
