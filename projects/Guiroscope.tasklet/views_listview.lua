d29267130ec5b4c5476a56188c4b7d7acfa76fbc
jomanto <jomanto@gmx.net>
2011-03-09 10:49:41 +0000

    added ListView demo

---
+++ projects/Guiroscope.tasklet/views_listview.lua
@@ -0,0 +1,151 @@
+local Color = lost.common.Color
+
+local loader = lost.resource.Loader.create()
+loader:addRepository(lost.resource.FilesystemRepository.create("/"))
+
+local datasource = {}
+for name in loader:directory("") do
+  table.insert(datasource, {name = name, path = tostring(name), padding = 0})
+end
+
+local directoryImage = lost.bitmap.Bitmap.create(tasklet.loader:load("img/blue-folder-horizontal.png"))
+directoryImage:premultiplyAlpha()
+local directoryOpenImage = lost.bitmap.Bitmap.create(tasklet.loader:load("img/blue-folder-horizontal-open.png"))
+directoryOpenImage:premultiplyAlpha()
+local rightImage = lost.bitmap.Bitmap.create(tasklet.loader:load("img/control-right.png"))
+rightImage:premultiplyAlpha()
+local downImage = lost.bitmap.Bitmap.create(tasklet.loader:load("img/control-down.png"))
+downImage:premultiplyAlpha()
+local documentImage = lost.bitmap.Bitmap.create(tasklet.loader:load("img/document.png"))
+documentImage:premultiplyAlpha()
+
+return lost.guiro.view.View
+{
+  sublayers =
+  {
+    lost.guiro.layer.Rect
+    {
+      filled = true,
+      color = Color(1,1,1)
+    }
+  },
+  subviews =
+  {
+    lost.guiro.view.ListView
+    {
+      alwaysShowCorner = true,
+      delegate =
+      {
+        createCellView = function(self)
+          local label = lost.guiro.view.Label {
+            bounds = {35,"center","1","1"},
+            font = {"Vera", 11},
+            text = "",
+            color = Color(0,0,0),
+            halign = "left"
+          }
+          local cell = lost.guiro.view.View {subviews = {label}}
+          cell.label = label
+          cell.images = images
+          cell.dataSource = function(self,ds)
+            if self.images == nil then
+              if boost.filesystem.is_directory(ds.name:status()) then
+                self.images = {
+                  control = {
+                    closed = lost.guiro.view.Image {
+                      bounds = {ds.padding,"center",16,16},
+                      flip = true,
+                      bitmap = rightImage,
+                      hidden = ds.data ~= nil
+                    },
+                    open = lost.guiro.view.Image {
+                      bounds = {ds.padding,"center",16,16},
+                      flip = true,
+                      bitmap = downImage,
+                      hidden = ds.data == nil
+                    }
+                  },
+                  directory = {
+                    closed = lost.guiro.view.Image {
+                      bounds = {ds.padding+16,"center",16,16},
+                      flip = true,
+                      bitmap = directoryImage,
+                      hidden = ds.data ~= nil
+                    },
+                    open = lost.guiro.view.Image {
+                      bounds = {ds.padding+16,"center",16,16},
+                      flip = true,
+                      bitmap = directoryOpenImage,
+                      hidden = ds.data == nil
+                    }
+                  }
+                }
+                self:add({self.images.control.closed, self.images.control.open, self.images.directory.closed, self.images.directory.open})
+                self.images.control.closed:addEventListener("mouseDown", function(event) self:toggle(ds) end)
+                self.images.control.open:addEventListener("mouseDown", function(event) self:toggle(ds) end)
+              else
+                self.images = {
+                  file = lost.guiro.view.Image {
+                    bounds = {ds.padding+16,"center",16,16},
+                    flip = true,
+                    bitmap = documentImage
+                  }
+                }
+                self:addSubview(self.images.file)
+              end
+            end
+            self.label:bounds(lost.guiro.Bounds(unpack({ds.padding+35,"center","1","1"})))
+            self.label:text(tostring(ds.name))
+          end
+          cell.toggle = function(self,ds)
+            if ds.data ~= nil then
+              ds.data = nil
+              self:superview():superview():reloadData()
+            else
+              ds.data = {}
+              for name in loader:directory(ds.path) do
+                table.insert(ds.data, {name = name, path = tostring(boost.filesystem.path(ds.path) / tostring(name)), padding = ds.padding+25})
+              end
+              self:superview():superview():reloadData()
+            end
+          end
+          return cell
+        end,
+        cellForRowAtIndexPath   = function(self,listView,indexPath) return self:createCellView() end,
+        heightForRowAtIndexPath = function(self,listView,indexPath) return 20 end
+      },
+      dataSource = {
+        data = datasource,
+        getCount = function(self,data)
+          local result = 0
+          for k,v in ipairs(data) do
+            result = result+1
+            if v.data ~= nil then
+              result = result + self:getCount(v.data)
+            end
+          end
+          return result
+        end,
+        numberOfRowsInSection = function(self,listView,sectionIndex) return self:getCount(self.data) end,
+        getData = function(self,data,index,current)
+          local result = nil
+          for k,v in ipairs(data) do
+            if current == index then
+              result = v
+            else
+              current = current+1
+              if v.data ~= nil then
+                result, current = self:getData(v.data, index, current)
+              end
+            end
+            if result ~= nil then
+              break
+            end
+          end
+          return result, current
+        end,
+        rowForIndexPath = function(self,listViewuitable,indexPath) return self:getData(self.data, indexPath[2], 1) end
+      }
+    }
+  }
+}
