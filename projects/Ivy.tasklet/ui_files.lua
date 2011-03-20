b011d581719334cd703f7133e7d8ff250be936b4
lobotony <lobotony666@yahoo.de>
2011-03-20 17:47:17 +0000

    added empty files screen

--- projects/Ivy.tasklet/ui_files.lua
+++ projects/Ivy.tasklet/ui_files.lua
@@ -1,4 +1,6 @@
 local Color = lost.common.Color
+local Vec2 = lost.math.Vec2
+fileListWidth = 200
 
 return lost.guiro.view.View
 {
@@ -7,9 +9,10 @@ return lost.guiro.view.View
     lost.guiro.view.ListView
     {
       sublayers={lost.guiro.layer.Rect{filled=true,color=Color(1,1,1)}},
-      bounds={0,0,200,"1"},
+      bounds={0,0,fileListWidth,"1"},
       delegate=fileListDelegate,
       dataSource=fileListDatasource
-    }
+    },
+    require("ui_files_empty")
   }
 }
