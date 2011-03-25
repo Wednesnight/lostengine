1a06221a230536d9aa936dc499d1f4df3492bdce
lobotony <lobotony666@yahoo.de>
2011-03-25 00:56:18 +0000

    started upgrade to Box2D 2.1.2: added new version, removed old, adjusted gyp file, started to rewrite bindings, not quite done yet, but SandBox2D does start up

--- projects/SandBox2D.tasklet/main.lua
+++ projects/SandBox2D.tasklet/main.lua
@@ -1,5 +1,15 @@
 function startup()
+  local gravity = box2d.b2Vec2(0,-10)
+  local doSleep = true
+  world = box2d.b2World(gravity, doSleep)
 end
 
 function update()
+  
+end
+
+function key(event)
+  if event.key == lost.application.K_ESCAPE then
+    tasklet.running = false
+  end
 end
