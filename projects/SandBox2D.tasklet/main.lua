55350713f864e409feac7240f8785ed186eca942
lobotony <lobotony666@yahoo.de>
2011-03-27 17:21:32 +0000

    most basic Box2D demo works

--- projects/SandBox2D.tasklet/main.lua
+++ projects/SandBox2D.tasklet/main.lua
@@ -1,7 +1,40 @@
 function startup()
-  local gravity = box2d.b2Vec2(0,-10)
-  local doSleep = true
+   gravity = box2d.b2Vec2(0,-10)
+   doSleep = true
   world = box2d.b2World(gravity, doSleep)
+   groundBodyDef = box2d.b2BodyDef()
+  groundBodyDef.position:Set(0.0,-10.0)
+   groundBody = world:CreateBody(groundBodyDef)
+   groundBox = box2d.b2PolygonShape()
+  groundBox:SetAsBox(50.0,10.0)
+  groundBody:CreateFixture(groundBox,0)
+  
+  bodyDef = box2d.b2BodyDef()
+  bodyDef.type = box2d.b2_dynamicBody
+  bodyDef.position:Set(0,4)
+  body = world:CreateBody(bodyDef)
+  dynamicBox = box2d.b2PolygonShape()
+  dynamicBox:SetAsBox(1,1)
+  
+  fixtureDef = box2d.b2FixtureDef()
+  fixtureDef:setShape(dynamicBox)
+--  fixtureDef.shape = dynamicBox
+  fixtureDef.density = 1
+  fixtureDef.friction = .3
+  
+  fixture = body:CreateFixture(fixtureDef)
+  
+  timeStep = 1/60
+  velocityIterations = 6
+  positionIterations = 2
+  
+  for i=1,60 do
+    world:Step(timeStep,velocityIterations,positionIterations)
+    world:ClearForces()
+    position = body:GetPosition()
+    angle = body:GetAngle()
+    log.debug(position.x.." "..position.y.." "..angle)
+  end
 end
 
 function update()
