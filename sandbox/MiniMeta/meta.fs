7ae0e0ea41d680a918b7d84ace2c3dd0dac076fa
lobotony <lobotony666@yahoo.de>
2011-03-31 19:11:34 +0000

    slightly reformatted, optimised and changed falloff exponent

--- sandbox/MiniMeta/meta.fs
+++ sandbox/MiniMeta/meta.fs
@@ -3,26 +3,18 @@ varying vec3 va0;
 
 float mb(vec3 pos, float rad, vec3 current)
 {
-  return rad/(pow(current.x-pos.x,2.0)+pow(current.y-pos.y,2.0)+pow(current.z-pos.z,2.0));
+  vec3 tmp = pow(current-pos,vec3(2.));
+  return rad/(tmp.x+tmp.y+tmp.z);
 }
 
 void main(void)
 {
-    float rad1 = .1;
-    vec3 pos1 = vec3(.2,.2,0);
+  float rad1 = .1;
+  vec3 pos1 = vec3(.2,.2,0);
 
-    float rad2 = .05;
-    vec3 pos2 = vec3(.7,.7,0);
+  float rad2 = .045;
+  vec3 pos2 = vec3(.7,.7,0);
 
-    float thresh = .96;
-//    float f = step(.96,mb(vec3(.7,.7,0),.4,va0))
-    float f = pow(mb(pos1,rad1,va0)+mb(pos2,rad2,va0),64.0);
-
-//    float mbv = clamp(mb(pos1,rad1,va0)+mb(pos2,rad2,va0),0.0,1.0);
-//    float fac = 32.0;
-//    float f = 1.0-pow(1.0-pow(mbv,fac),fac);
-
-//    float f = step(thresh,mbv);
-//  float f = step(thresh,mb(pos1,rad1,va0));
+  float f = pow(mb(pos1,rad1,va0)+mb(pos2,rad2,va0),80.0);
   gl_FragColor = color*f;
 }
