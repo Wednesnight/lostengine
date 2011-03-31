73cc8867dc9332b62bd3ffcc5f9236d7491a2fc8
lobotony <lobotony666@yahoo.de>
2011-03-31 14:42:56 +0000

    added MiniMeta test project, removed old stuff

--- sandbox/Ivy.tasklet/sizes.lua
+++
@@ -1,8 +0,0 @@
-mainBorder=10
-taskletIconSize = 38
-titleLabelWidth = 200
-titleBoxWidth = taskletIconSize+titleLabelWidth
-pathLabelHeight = 20
-pathLabelSpacing = 4 -- distance between pathLabel and first visible frame
-toolViewWidth = 250
-toolViewSpacing = 4 -- distance between image view and info section
--- sandbox/MoonBuggy/moonbuggy.h
+++
@@ -1,3 +0,0 @@
-void moonBuggy_init();
-void moonBuggy_input(int button, int state, int x, int y);
-void moonBuggy_update();
