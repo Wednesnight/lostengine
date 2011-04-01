d2094dffc972eca6454c150b70cc03e06c86c400
lobotony <lobotony666@yahoo.de>
2011-04-01 01:29:42 +0000

    trying to fix draw order and testing if there is a compromise when discarding fragments underneath a certain alpha threshold

--- sandbox/MiniMeta/Cube.lua
+++ sandbox/MiniMeta/Cube.lua
@@ -19,18 +19,37 @@ function Cube:constructor(args)
   self.planes = {}
   self.renderNode = lost.rg.Node.create()
   self:createXY(args)
+  self:createXZ(args)
+  self:createZY(args)
 end
 
-function Cube:createXY(args)
+function Cube:createXY(args) self:createPlanes("xy",args) end
+function Cube:createXZ(args) self:createPlanes("xz",args) end
+function Cube:createZY(args) self:createPlanes("zy",args) end
+function Cube:createPlanes(mode,args)
   local spaceStep=self.size/(self.numPlanes-1)
   local metaStep=1/(self.numPlanes-1)
   local curSpace = 0
   local curMeta = 0
+  local off = -(self.size/2)
+  local globalOffset = Vec3(off,off,off)
   for i=1,self.numPlanes do
-    local plane = meta.Plane{mode="xy",color=self.color,rect=Rect(0,0,self.size,self.size),meshOffset=curSpace,metaOffset=curMeta}
+    local plane = meta.Plane{
+        ["mode"]=mode,
+        color=self.color,
+        rect=Rect(0,0,self.size,self.size), -- must stay at 0,0, should really be a Vec2
+        offset = globalOffset,
+        meshOffset=curSpace,
+        metaOffset=curMeta}
     curSpace = curSpace + spaceStep
     curMeta = curMeta + metaStep
     self.renderNode:add(lost.rg.Draw.create(plane.mesh))
     table.insert(self.planes,plane)
+  end
+end
+
+function Cube:transform(tf)
+  for k,plane in pairs(self.planes) do
+    plane.mesh.transform = tf
   end
 end
