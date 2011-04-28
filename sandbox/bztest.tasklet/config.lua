33505772e9ddbdfe1d00f87271493824c9dae0e7
jomanto <jomanto@gmx.net>
2011-04-28 18:07:43 +0000

    bezier curve test project

---
+++ sandbox/bztest.tasklet/config.lua
@@ -0,0 +1,16 @@
+return {
+  tasklet = {
+    name = "bztest",
+    waitForEvents = true
+  },
+  window = {
+    title = "bztest",
+    x = 50,
+    y = 50,
+    width = 640,
+    height = 480
+  },
+  gl ={
+    vsync = true
+  }
+}
