97ace82d24b07323eb1c7e71a87783b3a72a2270
lobotony <lobotony666@yahoo.de>
2011-06-10 18:26:01 +0000

    renamed Prefs to PrefsManager

--- projects/lostengine/resources/lost/common/PrefsManager.lua
+++ projects/lostengine/resources/lost/common/PrefsManager.lua
@@ -3,20 +3,20 @@ module("lost.common", package.seeall)
 require("lost.common.Class")
 require("lost.common.Serializer")
 
-lost.common.Class "lost.common.Prefs" {}
+lost.common.Class "lost.common.PrefsManager" {}
 
 -- Shortcut for maintaining a prefs file in userdata directory.
 -- construct with table parameter:
 -- local prefs = Prefs{...}
 -- prefix : directory prefix, will be appended to userDataPath
 -- filename : will be appended to userdatapath + prefix
-function Prefs:constructor(t)
+function PrefsManager:constructor(t)
   self._prefix = t.prefix or "LostEngine"
   self._filename = t.filename or "prefs.lua"
   self._prefsFilePath = lost.platform.getUserDataPath() / self._prefix / self._filename
 end
 
-function Prefs:load()
+function PrefsManager:load()
   local result = {}
   if boost.filesystem.exists(self._prefsFilePath) then
     log.debug("prefs exits, loading")
@@ -33,7 +33,7 @@ function Prefs:load()
   return result
 end
 
-function Prefs:save(t)
+function PrefsManager:save(t)
   local s = lost.common.Serializer()
   local data = s:serialize(t)
   
