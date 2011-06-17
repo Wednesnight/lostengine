7f430f6b86cb4092b11e06776e00543fbe3b46fd
lobotony <lobotony666@yahoo.de>
2011-06-17 13:12:56 +0000

    obsolete

--- sandbox/apptest/lost/common/eastlStreamSupport.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <EASTL/string.h>
-
-std::ostream& operator << (std::ostream& os, const eastl::string& s)
-{
-  os << s.c_str();
-  return os;
-}
--- sandbox/apptest/lost/platform/Platform.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOST_PLATFORM_PLATFORM_H
-#define LOST_PLATFORM_PLATFORM_H
-
-#include "lost/platform/Type.h"
-#include "lost/platform/Time.h"
-#include "lost/platform/Paths.h"
-
-#endif
