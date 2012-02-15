35076c9548feaada92ae1a6cfe2071e77474f854
Timo Boll <jomanto@gmx.net>
2012-02-15 23:48:33 +0100

    implemented tthread::call_once

---
+++ thirdparty/tinythread/1_0/source/once.cpp
@@ -0,0 +1,42 @@
+#include "once.h"
+
+#if (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= MAC_OS_X_VERSION_10_5 || __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= __IPHONE_2_0)
+#  include <libkern/OSAtomic.h>
+#elif (defined(_WIN32_WINNT) && _WIN32_WINNT >= _WIN32_WINNT_WIN2K) || (defined(WINVER) && WINVER >= _WIN32_WINNT_WIN2K)
+#  include <windows.h>
+#endif
+
+namespace tthread {
+
+  bool __atomic_cas(once_flag old_value,
+                    once_flag new_value,
+                    volatile once_flag& ref)
+  {
+    
+#if (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= MAC_OS_X_VERSION_10_5 || __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= __IPHONE_2_0)
+    
+    return OSAtomicCompareAndSwapInt(old_value,
+                                     new_value,
+                                     &ref);
+    
+#elif (defined(_WIN32_WINNT) && _WIN32_WINNT >= _WIN32_WINNT_WIN2K) || (defined(WINVER) && WINVER >= _WIN32_WINNT_WIN2K)
+    
+    return InterlockedCompareExchange(&ref,
+                                      new_value,
+                                      old_value);
+    
+#elif (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) > 40100
+    
+    return __sync_val_compare_and_swap(&ref,
+                                       old_value,
+                                       new_value);
+    
+#else
+    
+#  error No implementation
+    
+#endif
+    
+  }
+
+}
--- thirdparty/tinythread/1_0/source/once.h
+++ thirdparty/tinythread/1_0/source/once.h
@@ -1,14 +1,28 @@
 #ifndef _TINYTHREAD_ONCE_H_
 #define _TINYTHREAD_ONCE_H_
 
-#include "tinythread.h"
+#define TTHREAD_ONCE_INIT  0
+#define TTHREAD_ONCE_FINAL 1
 
-namespace tinythread
+namespace tthread
 {
-  
-  
-  
-}
+#if (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= MAC_OS_X_VERSION_10_5 || __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= __IPHONE_2_0)
+  typedef int32_t once_flag;
+#elif (defined(_WIN32_WINNT) && _WIN32_WINNT >= _WIN32_WINNT_WIN2K) || (defined(WINVER) && WINVER >= _WIN32_WINNT_WIN2K)
+  typedef long once_flag;
+#elif (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) > 40100
+  typedef long once_flag;
+#endif
 
+  bool __atomic_cas(once_flag old_value,
+                    once_flag new_value,
+                    volatile once_flag& ref);
+
+  inline void call_once(void(*f)(), once_flag& flag) {
+    if (__atomic_cas(TTHREAD_ONCE_INIT, TTHREAD_ONCE_FINAL, flag)) {
+      f();
+    }
+  }
+}
 
 #endif
